019e0f47dd0b5ca74cb216ea3a36032bab0109b7
ismcalpi <ismcalpi@gmail.com>
2017-10-12 12:22:12 -0400

    Initial Commit

---
+++ include/navbar.php
@@ -0,0 +1,46 @@
+<!-- Header -->
+    <header id="js-header" class="u-header u-header--static--lg u-header--show-hide--lg u-header--change-appearance--lg" data-header-fix-moment="300" data-header-fix-effect="slide">
+      <div class="u-header__section u-header__section--light g-bg-white g-transition-0_3 g-py-10" data-header-fix-moment-exclude="g-bg-white g-py-10" data-header-fix-moment-classes="g-bg-white-opacity-0_7 u-shadow-v18 g-py-0">
+        <nav class="js-mega-menu navbar navbar-expand-lg">
+          <div class="container">
+            <!-- Responsive Toggle Button -->
+            <button class="navbar-toggler navbar-toggler-right btn g-line-height-1 g-brd-none g-pa-0 g-pos-abs g-top-3 g-right-0" type="button" aria-label="Toggle navigation" aria-expanded="false" aria-controls="navBar" data-toggle="collapse" data-target="#navBar">
+              <span class="hamburger hamburger--slider">
+            <span class="hamburger-box">
+              <span class="hamburger-inner"></span>
+              </span>
+              </span>
+            </button>
+            <!-- End Responsive Toggle Button -->
+            <!-- Logo -->
+            <a href="./" class="g-color-primary navbar-brand">
+				STORM | US-Stormrage
+            </a>
+            <!-- End Logo -->
+
+            <!-- Navigation -->
+            <div class="collapse navbar-collapse align-items-center flex-sm-row g-pt-10" id="navBar">
+              <ul class="navbar-nav ml-auto text-uppercase g-font-weight-600 u-main-nav-v5 u-sub-menu-v1">
+                <li class="nav-item g-mx-20--lg g-mb-5 g-mb-0--lg">
+                  <a href="#" class="nav-link">About Us</a>
+                </li>
+                <li class="nav-item g-mx-20--lg g-mb-5 g-mb-0--lg">
+                  <a href="#" class="nav-link">Videos</a>
+                </li>
+                <li class="nav-item g-mx-20--lg g-mb-5 g-mb-0--lg">
+                  <a href="#" class="nav-link">Streams</a>
+                </li>
+                <li class="nav-item g-mx-20--lg g-mb-5 g-mb-0--lg">
+                  <a target="_blank" href="https://www.warcraftlogs.com/guilds/145258" class="nav-link">Warcraft Logs</a>
+                </li>
+                <li class="nav-item g-mx-20--lg g-mb-5 g-mb-0--lg">
+                  <a href="#" class="nav-link">Recruitment</a>
+                </li>
+              </ul>
+            </div>
+            <!-- End Navigation -->
+          </div>
+        </nav>
+      </div>
+    </header>
+    <!-- End Header -->
---
+++ index.php
@@ -0,0 +1,297 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include './include/head.php' ?>
+
+<body>
+  <main>
+	   
+	<?php include './include/navbar.php' ?>
+		
+	<!-- Coming Soon Countdown -->
+	<section class="dzsparallaxer auto-init height-is-based-on-content">
+		<!-- Parallax Image -->
+		<div class="divimage dzsparallaxer--target w-100 g-bg-size-cover u-bg-overlay g-bg-black-opacity-0_5--after" style="height: 140%; background: url(./img/1920x800/wow01.jpg)"></div>
+		<!-- End Parallax Image -->
+
+		<div class="container u-bg-overlay__inner g-py-100">
+		  <div class="text-center g-color-white g-mb-60">
+			<h2 class="h1">Coming Soon!</h2>
+		  </div>
+
+			<div>
+				<div>
+				  <div class="js-countdown u-countdown-v5 g-color-white text-center" data-end-date="2017/11/04" data-days-format="%D" data-hours-format="%H" data-minutes-format="%M" data-seconds-format="%S">
+					<div class="d-inline-block g-mx-20 g-mb-40">
+					  <div class="js-cd-days g-font-size-56 g-font-weight-300 g-line-height-1"></div>
+					  <span>Days</span>
+					</div>
+
+					<div class="hidden-down d-inline-block align-top g-font-size-28 g-font-weight-200 g-line-height-1_8">|</div>
+
+					<div class="d-inline-block g-mx-20 g-mb-40">
+					  <div class="js-cd-hours g-font-size-56 g-font-weight-300 g-line-height-1"></div>
+					  <span>Hours</span>
+					</div>
+
+					<div class="hidden-down d-inline-block align-top g-font-size-28 g-font-weight-200 g-line-height-1_8">|</div>
+
+					<div class="d-inline-block g-mx-20 g-mb-40">
+					  <div class="js-cd-minutes g-font-size-56 g-font-weight-300 g-line-height-1"></div>
+					  <span>Minutes</span>
+					</div>
+
+					<div class="hidden-down d-inline-block align-top g-font-size-28 g-font-weight-200 g-line-height-1_8">|</div>
+
+					<div class="d-inline-block g-mx-20 g-mb-40">
+					  <div class="js-cd-seconds g-font-size-56 g-font-weight-300 g-line-height-1"></div>
+					  <span>Seconds</span>
+					</div>
+				  </div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- End Coming Soon Countdown -->
+	
+	<!-- Recruitment Alert -->
+	<div class="alert alert-dismissible fade show g-bg-gray-dark-v2 g-color-white rounded-0" role="alert">
+		<div class="container">
+			<!-- <button type="button" class="close u-alert-close--light" data-dismiss="alert" aria-label="Close">
+				<span class="g-color-white" aria-hidden="true">×</span>
+			</button> -->
+			<div class="media">
+				<span class="media-body align-self-center">
+					<strong class="h2">Recruiting |  </strong>
+					<!-- HPally -->
+					<img style="vertical-align:top;" height="40px" src="./img/spec_icons/holypaladin.png" title="Holy Paladin" />
+					<!-- Mist Monk -->
+					<img style="vertical-align:top;" height="40px" src="./img/spec_icons/mistweaver.png" title="Mistweaver Monk" />
+				</span>
+			</div>
+		</div>
+	</div>
+    <!-- End Recruitment Alert -->
+	
+	<!-- News Feed -->
+	<div style="padding-top:80px;" class="container u-shadow-v1-3 g-line-height-2 g-pa-40 g-mb-30">
+	
+		<!-- Progression -->
+		<div class="u-heading-v1-7 g-bg-main g-brd-gray-light-v2 g-mb-20">
+			<h2 class="h3 u-heading-v1__title">Raid Progression</h2>
+		</div>
+		<div class="row">
+			<!-- Article -->
+			<div class="col-lg-12 g-px-0--md g-mb-30">
+				<article class="u-block-hover">
+					<figure style="max-height:125px;" class="u-bg-overlay g-bg-black-gradient-opacity-v1--after">
+						<img class="img-fluid" src="./img/raid_bg/tos.jpg">
+					</figure>
+					<div class="progress g-pos-abs g-right-20 g-bottom-10 g-left-20">
+						<div class="progress-bar g-height-20" role="progressbar" aria-valuenow="88" aria-valuemin="0" aria-valuemax="100" style="width: 88%;">8/9</div>
+					</div>
+					<span class="g-pos-abs g-top-20 g-right-20">
+						<a class="btn btn-sm u-btn-aqua rounded-0" href="#">Mythic</a>
+					</span>
+				</article>
+				<header class="g-pos-abs g-right-20 g-top-10 g-left-20">
+					<h3 class="h5 g-font-weight-600 g-color-white">
+					  Tomb of Sargeras
+					</h3>
+				</header>
+			</div>
+			<!-- End Article -->
+			<!-- Article -->
+			<div class="col-lg-4 g-px-0--md g-mb-30">
+			<article class="u-block-hover">
+				<figure style="max-height:125px;" class="u-bg-overlay g-bg-black-gradient-opacity-v1--after">
+					<img class="img-fluid" src="./img/raid_bg/en.jpg">
+				</figure>
+				<div class="progress g-pos-abs g-right-20 g-bottom-10 g-left-20">
+					<div class="progress-bar g-height-20" role="progressbar" aria-valuenow="7" aria-valuemin="0" aria-valuemax="7" style="width: 100%;">7/7</div>
+				</div>
+				<span class="g-pos-abs g-top-20 g-right-20">
+					<a class="btn btn-sm u-btn-aqua rounded-0" href="#">Mythic</a>
+				</span>
+			</article>
+			<header class="g-pos-abs g-right-20 g-top-10 g-left-20">
+				<h3 class="h5 g-font-weight-600 g-color-white">
+				  Emerald Nightmare
+				</h3>
+			</header>
+			</div>
+			<!-- End Article -->
+			<!-- Article -->
+			<div class="col-lg-4 g-px-0--md g-mb-30">
+			<article class="u-block-hover">
+				<figure style="max-height:125px;" class="u-bg-overlay g-bg-black-gradient-opacity-v1--after">
+					<img class="img-fluid" src="./img/raid_bg/tov.jpg">
+				</figure>
+				<div class="progress g-pos-abs g-right-20 g-bottom-10 g-left-20">
+					<div class="progress-bar g-height-20" role="progressbar" aria-valuenow="3" aria-valuemin="0" aria-valuemax="3" style="width: 100%;">3/3</div>
+				</div>
+				<span class="g-pos-abs g-top-20 g-right-20">
+					<a class="btn btn-sm u-btn-aqua rounded-0" href="#">Mythic</a>
+				</span>
+			</article>
+			<header class="g-pos-abs g-right-20 g-top-10 g-left-20">
+				<h3 class="h4 g-font-weight-600 g-color-white">
+				  Trial of Valor
+				</h3>
+			</header>
+			</div>
+			<!-- End Article -->
+			<!-- Article -->
+			<div class="col-lg-4 g-px-0--md g-mb-30">
+			<article class="u-block-hover">
+				<figure style="max-height:125px;" class="u-bg-overlay g-bg-black-gradient-opacity-v1--after">
+					<img class="img-fluid" src="./img/raid_bg/nh.jpg">
+				</figure>
+				<div class="progress g-pos-abs g-right-20 g-bottom-10 g-left-20">
+					<div class="progress-bar g-height-20" role="progressbar" aria-valuenow="10" aria-valuemin="0" aria-valuemax="10" style="width: 100%;">10/10</div>
+				</div>
+				<span class="g-pos-abs g-top-20 g-right-20">
+					<a class="btn btn-sm u-btn-aqua rounded-0" href="#">Mythic</a>
+				</span>
+			</article>
+			<header class="g-pos-abs g-right-20 g-top-10 g-left-20">
+				<h3 class="h4 g-font-weight-600 g-color-white">
+				  Nighthold
+				</h3>
+			</header>
+			</div>
+			<!-- End Article -->
+		</div>
+		<!-- End Progression -->
+		
+		<!-- Boss Kills -->
+		<div class="u-heading-v1-7 g-bg-main g-brd-gray-light-v2 g-mb-20">
+			<h2 class="h3 u-heading-v1__title">News</h2>
+		</div>
+		<div class="row">
+			<div class="col-lg-6 g-mb-30">
+				<!-- Article -->
+				<div class="u-shadow-v1-5 g-mb-30">
+					<article class="u-block-hover">
+						<a class="js-fancybox d-block u-block-hover" href="https://www.youtube.com/watch?v=eN3lI4mOUlk" title="Avatar Down!">
+							<img class="img-fluid" src="./img/boss_kills/avatar.jpg">
+							<span class="u-block-hover__additional--fade g-bg-black-opacity-0_8 g-color-white">
+								<i class="hs-icon hs-icon-play g-absolute-centered g-font-size-25"></i>
+							</span>
+						</a>
+						<header class="g-pos-abs g-right-20 g-bottom-10 g-left-20">
+							<span class="g-color-white">
+								<i class="icon-clock g-pos-rel g-top-1 g-mr-2"></i>
+								Sep 28th, 2017
+								</span>
+							<h3 class="h2 g-font-weight-600">
+							  <a class="g-color-white" href="#">Mythic Avatar Down!</a>
+							</h3>
+						</header>
+					</article>
+					
+				</div>
+			</div>
+			<!-- End Article -->
+			<!-- Article -->
+			<div class="col-lg-6 g-mb-30">
+				<div class="u-shadow-v1-5 g-mb-30">
+					<article class="u-block-hover">
+						<a class="js-fancybox d-block u-block-hover" href="./img/boss_kills/maiden.jpg">
+							<img class="img-fluid" src="./img/boss_kills/maiden.jpg">
+							<span class="u-block-hover__additional--fade g-bg-black-opacity-0_8 g-color-white">
+								<i class="hs-icon hs-icon-plus g-absolute-centered g-font-size-25"></i>
+							</span>
+						</a>
+						<header class="g-pos-abs g-right-20 g-bottom-10 g-left-20">
+							<span class="g-color-white">
+								<i class="icon-clock g-pos-rel g-top-1 g-mr-2"></i>
+								Sep 10th, 2017
+								</span>
+							<h3 class="h2 g-font-weight-600">
+							  <a class="g-color-white" href="#">Mythic Maiden Down!</a>
+							</h3>
+						</header>
+					</article>
+				</div>
+			</div>
+			<!-- End Article -->
+			<!-- Article -->
+			<div class="col-lg-6 g-mb-30">
+				<div class="u-shadow-v1-5 g-mb-30">
+					<article class="u-block-hover">
+						<a class="js-fancybox d-block u-block-hover" href="./img/boss_kills/mistress.jpg">
+							<img class="img-fluid" src="./img/boss_kills/mistress.jpg">
+							<span class="u-block-hover__additional--fade g-bg-black-opacity-0_8 g-color-white">
+								<i class="hs-icon hs-icon-plus g-absolute-centered g-font-size-25"></i>
+							</span>
+						</a>
+						<header class="g-pos-abs g-right-20 g-bottom-10 g-left-20">
+							<span class="g-color-white">
+								<i class="icon-clock g-pos-rel g-top-1 g-mr-2"></i>
+								Aug 18th, 2017
+								</span>
+							<h3 class="h2 g-font-weight-600">
+							  <a class="g-color-white" href="#">Mythic Mistress Sassz'ine Down!</a>
+							</h3>
+						</header>
+					</article>
+				</div>
+			</div>
+			<!-- End Article -->
+			<!-- Article -->
+			<div class="col-lg-6 g-mb-30">
+				<div class="u-shadow-v1-5 g-mb-30">
+					<article class="u-block-hover">
+						<a class="js-fancybox d-block u-block-hover" href="./img/boss_kills/desolatehost.jpg">
+							<img class="img-fluid" src="./img/boss_kills/desolatehost.jpg">
+							<span class="u-block-hover__additional--fade g-bg-black-opacity-0_8 g-color-white">
+								<i class="hs-icon hs-icon-plus g-absolute-centered g-font-size-25"></i>
+							</span>
+						</a>
+						<header class="g-pos-abs g-right-20 g-bottom-10 g-left-20">
+							<span class="g-color-white">
+								<i class="icon-clock g-pos-rel g-top-1 g-mr-2"></i>
+								Jul 6th, 2017
+								</span>
+							<h3 class="h2 g-font-weight-600">
+							  <a class="g-color-white" href="#">Mythic Desolate Host Down!</a>
+							</h3>
+						</header>
+					</article>
+				</div>
+			</div>
+			<!-- End Article -->
+		</div>
+		<!-- End Boss Kills -->
+		
+	</div>  
+	<!-- End News Feed -->
+	
+  </main>
+
+  <?php include './include/scripts.php' ?>
+  
+	<!-- JS Plugins Init. -->
+	<script >
+		$(document).on('ready', function () {
+			// initialization of countdowns
+			$.HSCore.components.HSPopup.init('.js-fancybox');
+			
+			var countdowns = $.HSCore.components.HSCountdown.init('.js-countdown', {
+				yearsElSelector: '.js-cd-years',
+				monthElSelector: '.js-cd-month',
+				daysElSelector: '.js-cd-days',
+				hoursElSelector: '.js-cd-hours',
+				minutesElSelector: '.js-cd-minutes',
+				secondsElSelector: '.js-cd-seconds'
+			});
+		});
+	</script>
+  
+</body>
+
+</html>
+
+
+	
