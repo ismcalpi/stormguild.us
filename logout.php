29535d738dde8a4d5c8a60692203fa6465deb43b
ismcalpi <ismcalpi@gmail.com>
2017-11-14 11:13:10 -0500

    Bug - Can't Logout
    * Updated the logout function to unset the cookie values.

--- logout.php
+++ logout.php
@@ -2,6 +2,11 @@
 
    session_start();
 
+   setcookie('username', "", time() + (86400 * 30), "/");
+   setcookie('user_rank', "", time() + (86400 * 30), "/");
+
+   unset($_COOKIE["username"]);
+   unset($_COOKIE["user_rank"]);
    unset($_SESSION["username"]);
    unset($_SESSION["password"]);
    unset($_SESSION['user_rank']);
