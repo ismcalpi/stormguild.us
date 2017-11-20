78ef5dc56bdf6aa5370004611131926f6bc20151
usumcia <imcalpine@scor.com>
2017-11-20 11:10:03 -0500

    Style Update
    * Added in Master Slider to All
    * Need to create Headers for each individual page.

--- templates/all.header.php
+++ templates/all.header.php
@@ -19,16 +19,9 @@
 
 	<!-- CSS Global Icons -->
 	<link rel="stylesheet" href="assets/vendor/icon-awesome/css/font-awesome.min.css">
-	<link rel="stylesheet" href="assets/vendor/icon-line/css/simple-line-icons.css">
-	<link rel="stylesheet" href="assets/vendor/icon-etlinefont/style.css">
-	<link rel="stylesheet" href="assets/vendor/icon-line-pro/style.css">
 	<link rel="stylesheet" href="assets/vendor/icon-hs/style.css">
-	<link rel="stylesheet" href="assets/vendor/dzsparallaxer/dzsparallaxer.css">
-	<link rel="stylesheet" href="assets/vendor/dzsparallaxer/dzsscroller/scroller.css">
-	<link rel="stylesheet" href="assets/vendor/dzsparallaxer/advancedscroller/plugin.css">
 	<link rel="stylesheet" href="assets/vendor/slick-carousel/slick/slick.css">
 	<link rel="stylesheet" href="assets/vendor/animate.css">
-	<link rel="stylesheet" href="assets/vendor/hs-megamenu/src/hs.megamenu.css">
 	<link rel="stylesheet" href="assets/vendor/hamburgers/hamburgers.min.css">
 
 	<!-- CSS Unify -->
@@ -37,8 +30,11 @@
 	<link rel="stylesheet" href="assets/css/unify-globals.css">
 
 	<!-- CSS Implementing Plugins -->
-    <link  rel="stylesheet" href="assets/vendor/fancybox/jquery.fancybox.min.css">
-    <link  rel="stylesheet" href="assets/vendor/jquery-ui/themes/base/jquery-ui.min.css">
+  <link  rel="stylesheet" href="assets/vendor/fancybox/jquery.fancybox.min.css">
+  <link  rel="stylesheet" href="assets/vendor/jquery-ui/themes/base/jquery-ui.min.css">
+
+	<!-- Master Slider -->
+	<link rel="stylesheet" href="assets/vendor/master-slider/source/assets/css/masterslider.main.css">
 
 	<!-- CSS Customization -->
 	<link rel="stylesheet" href="assets/css/custom.css">
