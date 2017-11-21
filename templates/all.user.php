e59ef822eefda6291efac6990f8d7c3d1c47461e
ismcalpi <ismcalpi@gmail.com>
2017-11-20 19:15:40 -0500

    Update

--- templates/all.user.php
+++ templates/all.user.php
@@ -1,24 +1,12 @@
 <?php
 define('IN_PHPBB', true);
-define('ROOT_PATH', "forums");
+$phpbb_root_path = (defined('PHPBB_ROOT_PATH')) ? PHPBB_ROOT_PATH : './';
+$phpEx = substr(strrchr(__FILE__, '.'), 1);
+include($phpbb_root_path . 'common.' . $phpEx);
 
-if (!defined('IN_PHPBB') || !defined('ROOT_PATH')) {
-    exit();
-}
-
-if (is_dir('forums')) {
-  $phpEx = "php";
-  $phpbb_root_path = (defined('PHPBB_ROOT_PATH')) ? PHPBB_ROOT_PATH : ROOT_PATH . '/';
-  include($phpbb_root_path . 'common.' . $phpEx);
-
-  $request->enable_super_globals();
-  
-  $user->session_begin();
-  $auth->acl($user->data);
-  $user->setup();
-
-} else {
-  echo 'ERROR: No Forums installed';
-}
-
+// Start session management
+$user->session_begin();
+$auth->acl($user->data);
+$user->setup();
+$request->enable_super_globals();
 ?>
