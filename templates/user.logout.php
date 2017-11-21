cb9d71ae00bc96fd9c1f70b565565da8be2dc5af
ismcalpi <ismcalpi@gmail.com>
2017-11-20 23:11:05 -0500

    Rethinking the Logout Function

--- templates/user.logout.php
+++ templates/user.logout.php
@@ -1,21 +1,11 @@
 <?php
-
 $user->session_kill();
 $user->session_begin();
-
-header( "refresh:3;url=index.php" );
-
 ?>
-
-<!-- Red Alert -->
-<div class="alert alert-dismissible fade show g-bg-red g-color-white rounded-0" role="alert">
-  <div class="media">
-    <span class="d-flex g-mr-10 g-mt-5">
-      <i class="icon-ban g-font-size-25"></i>
-    </span>
-    <span class="media-body align-self-center">
-      You have been successfully logged out. Redirecting home in 3 seconds...
-    </span>
+<!-- Form -->
+<div class="row justify-content-center">
+  <div class="col-md-6">
+    <h2 class="h2 g-color-primary">You have been successfully logged out!</h2>
   </div>
 </div>
-<!-- End Red Alert -->
+<!-- End Form -->
