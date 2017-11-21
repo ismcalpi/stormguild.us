f01442e316c80ef7107f1288f7e7afc7c498aeae
usumcia <imcalpine@scor.com>
2017-11-21 14:25:29 -0500

    * Formatting for login change * add register to user. Not using right now.

--- templates/user.login.php
+++ templates/user.login.php
@@ -2,11 +2,11 @@
 <div class="row justify-content-center">
   <div class="col-md-6">
     <form class="g-py-15" method="POST" action="forums/ucp.php?mode=login">
-      <div class="mb-4">
+      <div class="g-mb-10">
         <input class="form-control g-color-black g-bg-white g-bg-white--focus g-brd-gray-light-v4 g-brd-primary--hover rounded g-py-15 g-px-15"
           type="text" placeholder="Username" name="username">
       </div>
-      <div class="g-mb-35">
+      <div class="g-mb-10">
         <input class="form-control g-color-black g-bg-white g-bg-white--focus g-brd-gray-light-v4 g-brd-primary--hover rounded g-py-15 g-px-15 mb-3"
           type="password" placeholder="Password" name="password">
         <div class="row justify-content-between">
