28c6c866adfc80752f2ef78dfa5e1936fe59c033
ismcalpi <ismcalpi@gmail.com>
2017-11-24 13:16:15 -0500

    De-Debug

--- templates/admin.application.php
+++ templates/admin.application.php
@@ -1,14 +1,14 @@
 <div class="row">
-  <div class="col-8">
-    <?php include_once $_SERVER['DOCUMENT_ROOT'].'/templates/admin.application.navbar.php' ?>
+  <div class="col-3">
+    <?php include $_SERVER['DOCUMENT_ROOT'].'/templates/admin.application.navbar.php' ?>
   </div>
   <div class="col-9">
     <div class="row">
       <div class="col-lg-8">
-        <?php #include 'templates/admin.application.body' ?>
+        <?php include $_SERVER['DOCUMENT_ROOT'].'/templates/admin.application.body' ?>
       </div>
       <div class="col-lg-4">
-        <?php #include 'templates/admin.application.comments' ?>
+        <?php include $_SERVER['DOCUMENT_ROOT'].'/templates/admin.application.comments' ?>
       </div>
   </div>
 </div>
