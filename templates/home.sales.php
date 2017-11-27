df0a66cb60a9ac4277cc8bffe4c40ae36fc4632e
usumcia <imcalpine@scor.com>
2017-11-27 15:53:28 -0500

    Update Sales Section

--- templates/home.sales.php
+++ templates/home.sales.php
@@ -5,7 +5,7 @@
             Loot & Achievement Sales!
           </h4>
             <a class="btn btn-md u-btn-outline-white g-font-weight-600 g-font-size-11 text-uppercase g-my-10" href="#">
-              Contact Us!
+              Contact: Aalistor#1455 or Lokilok#1587
             </a>
         </div>
       </div>
