d081c6f2ba79e2df1589bb4fa26a3c0d9ccef9ba
ismcalpi <ismcalpi@gmail.com>
2017-11-27 18:58:33 -0500

    Fix Admin Formatting

--- templates/application.admin.php
+++ templates/application.admin.php
@@ -3,25 +3,26 @@
   <?php
     $result = $db -> sql_fetchrow("SELECT * FROM stormguild.application WHERE application_id = ".$appid);
   ?>
-  <form id="comForm" method="post" action="library/action.application.decision.php" class="col-4">
+  <h3 class="h3 col">Admin Control:</h3>
+  <form method="post" action="library/action.application.decision.php" class="col">
       <div class="form-group g-mb-20">
           <input type="hidden" name="redirecturi" value="<?php echo $_SERVER['REQUEST_URI'] ?>" />
           <input type="hidden" name="appid" value="<?php echo $appid ?>" />
           <input type="hidden" name="appname" value="<?php echo $result['charName'] ?>" />
           <input type="hidden" name="status" value="accepted" />
-          <button type="submit" class="btn btn-md u-btn-inset u-btn-outline-blue g-mr-10 g-my-15">
+          <button type="submit" class="btn btn-md u-btn-inset u-btn-outline-primary g-mr-10 g-my-15">
               Accept Application
           </button>
       </div>
   </form>
-  <form id="comForm" method="post" action="library/action.application.decision.php" class="col-4">
+  <form method="post" action="library/action.application.decision.php" class="col">
       <div class="form-group g-mb-20">
           <input type="hidden" name="redirecturi" value="<?php echo $_SERVER['REQUEST_URI'] ?>" />
           <input type="hidden" name="appid" value="<?php echo $appid ?>" />
           <input type="hidden" name="appname" value="<?php echo $result['charName'] ?>" />
           <input type="hidden" name="status" value="declined" />
-          <button type="submit" class="btn btn-md u-btn-inset u-btn-outline-blue g-mr-10 g-my-15">
-              Accept Application
+          <button type="submit" class="btn btn-md u-btn-inset u-btn-outline-red g-mr-10 g-my-15">
+              Decline Application
           </button>
       </div>
   </form>
