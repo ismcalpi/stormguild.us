4d577135087cad857014cb0bbc3802bbd28402b2
ismcalpi <ismcalpi@gmail.com>
2017-11-27 19:17:45 -0500

    Fix

--- templates/application.admin.php
+++ templates/application.admin.php
@@ -1,9 +1,9 @@
 <?php if (!empty($appid)) { ?>
-<div class="row g-brd-around g-brd-black g-brd-1 g-pa-10">
+<div class="row g-brd-bottom g-brd-black g-brd-1 g-pa-10">
   <?php
     $result = $db -> sql_fetchrow("SELECT * FROM stormguild.application WHERE application_id = ".$appid);
   ?>
-  <h3 class="h3 g-mr-10">Admin Control:</h3>
+  <h3 class="h3 g-mr-10 g-my-15">Admin Control:</h3>
   <form method="post" action="library/action.application.decision.php">
       <div class="form-group g-mr-10">
           <input type="hidden" name="redirecturi" value="<?php echo $_SERVER['REQUEST_URI'] ?>" />
