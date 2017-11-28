30603bc396f4d82e708dfbffd0ad9193f5922d8a
ismcalpi <ismcalpi@gmail.com>
2017-11-27 19:01:27 -0500

    Admin Control for Application Formating

--- templates/application.admin.php
+++ templates/application.admin.php
@@ -1,16 +1,16 @@
 <?php if (!empty($appid)) { ?>
-<div class="row">
+<div class="row g-brd-around g-brd-black">
   <?php
     $result = $db -> sql_fetchrow("SELECT * FROM stormguild.application WHERE application_id = ".$appid);
   ?>
   <h3 class="h3 col">Admin Control:</h3>
-  <form method="post" action="library/action.application.decision.php" class="col">
+  <form method="post" action="library/action.application.decision.php" class="col g-mr-10 g-my-15">
       <div class="form-group g-mb-20">
           <input type="hidden" name="redirecturi" value="<?php echo $_SERVER['REQUEST_URI'] ?>" />
           <input type="hidden" name="appid" value="<?php echo $appid ?>" />
           <input type="hidden" name="appname" value="<?php echo $result['charName'] ?>" />
           <input type="hidden" name="status" value="accepted" />
-          <button type="submit" class="btn btn-md u-btn-inset u-btn-outline-primary g-mr-10 g-my-15">
+          <button type="submit" class="btn btn-md u-btn-outline-primary g-mr-10 g-my-15">
               Accept Application
           </button>
       </div>
@@ -21,7 +21,7 @@
           <input type="hidden" name="appid" value="<?php echo $appid ?>" />
           <input type="hidden" name="appname" value="<?php echo $result['charName'] ?>" />
           <input type="hidden" name="status" value="declined" />
-          <button type="submit" class="btn btn-md u-btn-inset u-btn-outline-red g-mr-10 g-my-15">
+          <button type="submit" class="btn btn-md u-btn-outline-red g-mr-10 g-my-15">
               Decline Application
           </button>
       </div>
