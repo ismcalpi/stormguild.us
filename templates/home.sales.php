c0a0be010b220bfac39f263b29839bc9747de3b6
usumcia <imcalpine@scor.com>
2017-11-28 11:20:44 -0500

    Update Sales

--- templates/home.sales.php
+++ templates/home.sales.php
@@ -1,12 +1,24 @@
 <article class="text-center g-color-white g-overflow-hidden g-rounded-top-5" >
-     <div class="u-block-hover--scale g-min-height-100 g-flex-middle g-bg-cover g-bg-bluegray-opacity-0_3--after g-transition-0_5" style="background-image: url('assets/img/home.sales.jpg');">
+     <div class="u-block g-min-height-100 g-flex-middle g-bg-cover g-bg-bluegray-opacity-0_3--after g-transition-0_5" style="background-image: url('assets/img/home.sales.jpg');">
         <div class="g-flex-middle-item g-pos-rel g-z-index-1 g-py-50 g-px-20 bg-black-0-70">
           <h4 class="text-uppercase">
             Loot & Achievement Sales!
           </h4>
-            <a class="btn btn-md u-btn-outline-white g-font-weight-600 g-font-size-11 text-uppercase g-my-10" href="#">
-              Contact: Aalistor#1455 or Lokilok#1587
-            </a>
+          <ul class="u-list-inline">
+            <li class="list-inline-item g-mb-10">
+              <h5 class="h5 text-center text-uppercase">Contact: </h5>
+            </li>
+            <li class="list-inline-item g-mb-10">
+              <a class="u-tags-v1 g-color-main g-brd-around g-brd-gray-light-v3 g-bg-gray-dark-v2--hover g-brd-gray-dark-v2--hover g-color-white--hover g-py-4 g-px-10" href="#">
+                Aalistor#1455
+              </a>
+            </li>
+            <li class="list-inline-item g-mb-10">
+              <a class="u-tags-v1 g-color-main g-brd-around g-brd-gray-light-v3 g-bg-gray-dark-v2--hover g-brd-gray-dark-v2--hover g-color-white--hover g-py-4 g-px-10" href="#">
+                Lokilok#1587
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
   </article>
