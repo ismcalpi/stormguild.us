efa93eb48fbddf2f92a05999bddbb1c74b939f2c
usumcia <imcalpine@scor.com>
2017-11-28 12:20:50 -0500

    Pretty

--- templates/home.sales.php
+++ templates/home.sales.php
@@ -1,6 +1,6 @@
 <article class="text-center g-color-white g-overflow-hidden g-rounded-top-5" >
      <div class="u-block g-min-height-100 g-flex-middle g-bg-cover g-bg-bluegray-opacity-0_3--after g-transition-0_5" style="background-image: url('assets/img/home.sales.jpg');">
-        <div class="g-flex-middle-item g-pos-rel g-z-index-1 g-py-50 g-px-20 bg-black-0-70">
+        <div class="g-flex-middle-item g-pos-rel g-z-index-1 g-py-10 g-px-10 bg-black-0-70">
           <h4 class="text-uppercase">
             Loot & Achievement Sales!
           </h4>
@@ -9,12 +9,12 @@
               <p><strong>Contact: </strong></p>
             </li>
             <li class="list-inline-item g-mb-10">
-              <a class="u-tags-v1 g-color-primary g-brd-around g-brd-primary g-bg-primary-opacity-0_1 g-bg-primary--hover g-color-white--hover g-py-4 g-px-10" href="#">
+              <a class="u-tags-v1 g-color-blue g-brd-around g-brd-blue g-bg-blue-opacity-0_1 g-bg-blue--hover g-color-white--hover g-py-4 g-px-10" href="#">
                 Aalistor#1455
               </a>
             </li>
             <li class="list-inline-item g-mb-10">
-              <a class="u-tags-v1 g-color-primary g-brd-around g-brd-primary g-bg-primary-opacity-0_1 g-bg-primary--hover g-color-white--hover g-py-4 g-px-10" href="#">
+              <a class="u-tags-v1 g-color-blue g-brd-around g-brd-blue g-bg-blue-opacity-0_1 g-bg-blue--hover g-color-white--hover g-py-4 g-px-10" href="#">
                 Lokilok#1587
               </a>
             </li>
@@ -23,7 +23,7 @@
         	$json = json_decode(file_get_contents('https://www.wowprogress.com/guild/us/stormrage/storm/json_rank'));
         	?>
           <a  href="https://www.wowprogress.com/guild/us/stormrage/storm"
-              class="btn btn-lg u-btn-bluegray u-btn-content g-font-weight-600 g-letter-spacing-0_5 text-uppercase g-brd-2 g-ma-5 g-pa-0">
+              class="btn btn-xl u-btn-bluegray u-btn-content g-font-weight-600 g-letter-spacing-0_5 text-uppercase g-brd-2 g-ma-5 g-pa-0">
             <i class="fa fa-globe pull-left g-font-size-25 g-my-10"></i>
             <span class="float-right text-left g-font-size-14">
               <span class="d-block g-font-size-11">wowprogress ranking</span>
