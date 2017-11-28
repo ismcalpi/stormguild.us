e660fee3a70e766cc4bf8631cd5d63de251f1974
ismcalpi <ismcalpi@gmail.com>
2017-11-27 20:09:00 -0500

    Fix

--- templates/admin.recruitneeds.php
+++ templates/admin.recruitneeds.php
@@ -3,7 +3,7 @@ include_once 'library/class.database.php';
 $db = new database();
 
 if(!empty($_POST['activate']) && !empty($_POST['id'])) {
-    $db-> query('UPDATE stormguild.recruitment SET is_active='.$_POST['activate'].' WHERE recruitment_id='.$_POST['id']);
+    $db-> sql_query('UPDATE stormguild.recruitment SET is_active='.$_POST['activate'].' WHERE recruitment_id='.$_POST['id']);
 }
 ?>
 <div class="row">
@@ -15,10 +15,12 @@ if(!empty($_POST['activate']) && !empty($_POST['id'])) {
       <table class="table text-center">
         <tbody>
           <?php
-            $classes = $db -> select("SELECT DISTINCT class_name FROM stormguild.recruitment order by class_name asc");
-            foreach($classes as $class) {
-              echo "<tr><td>".$class['class_name']."</td>";
-              $specs = $db -> select("SELECT spec_name, recruitment_id, is_active FROM stormguild.recruitment WHERE class_name ='".$class['class_name']."' order by spec_name asc");
+            $classes = $db -> sql_select("SELECT DISTINCT class_name FROM stormguild.recruitment order by class_name asc");
+            foreach($classes as $class) { ?>
+              <tr>
+                <td><?php echo $class['class_name'] ?></td>
+              <?php
+              $specs = $db -> sql_select("SELECT spec_name, recruitment_id, is_active FROM stormguild.recruitment WHERE class_name ='".$class['class_name']."' order by spec_name asc");
                 foreach($specs as $spec) {
                   if($spec['is_active'] == TRUE) {
                   ?>
