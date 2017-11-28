ee799b5b41730d78bce46352d31297039b989bc1
usumcia <imcalpine@scor.com>
2017-11-28 11:38:14 -0500

    Make stuff look better

--- templates/home.sales.php
+++ templates/home.sales.php
@@ -6,7 +6,7 @@
           </h4>
           <ul class="u-list-inline">
             <li class="list-inline-item g-mb-10">
-              <h5 class="h5 text-center text-uppercase">Contact: </h5>
+              <p><strong>Contact: </strong></p>
             </li>
             <li class="list-inline-item g-mb-10">
               <a class="u-tags-v1 g-color-main g-brd-around g-brd-gray-light-v3 g-bg-gray-dark-v2--hover g-brd-gray-dark-v2--hover g-color-white--hover g-py-4 g-px-10" href="#">
@@ -25,11 +25,18 @@
           	?>
       		<div class="g-pa-30 g-ma-0">
       			<a href='https://www.wowprogress.com/guild/us/stormrage/storm' target='_blank'>
-              <h5 class="h5 g-pos-abs g-left-40 g-bottom-10">
+              <h5 class="h5 text-center g-color-white">
                 <strong><u>WoW Progress Ranking</u></strong><br /> US Rank #<?php echo $json->area_rank; ?> Realm Rank #<?php echo $json->realm_rank; ?>
               </h5>
             </a>
       		</div>
+          <a href="#" class="btn btn-xl u-btn-outline-black u-btn-content g-font-weight-600 g-letter-spacing-0_5 text-uppercase g-brd-2 g-mr-10 g-mb-15">
+            <i class="fa fa-globe pull-left g-font-size-42 g-mr-15"></i>
+            <span class="float-right text-left">
+              <span class="d-block g-font-size-11">wowprogress ranking</span>
+              US Rank #<?php echo $json->area_rank; ?> Realm Rank #<?php echo $json->realm_rank; ?>
+            </span>
+          </a>
         </div>
       </div>
   </article>
