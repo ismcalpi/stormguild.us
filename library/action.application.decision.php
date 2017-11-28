74b06d9bd7b314786e00f738ffe674880e3e4ec9
ismcalpi <ismcalpi@gmail.com>
2017-11-27 19:35:01 -0500

    Add Admin control of Apps

--- library/action.application.decision.php
+++ library/action.application.decision.php
@@ -4,20 +4,19 @@ include($phpbb_root_path . 'includes/functions_messenger.' . $phpEx);
 include($phpbb_root_path . 'config.' . $phpEx);
 include_once $_SERVER['DOCUMENT_ROOT'].'/library/class.database.php';
 
-comment_add();
+update_app();
 
-$app_link = "https://www.stormguild.us/application.php?accessid=".$_POST['accessid'];
 $member_link = "https://www.stormguild.us/application.php?appid=".$_POST['appid'];
-$discord_msg = "New Comment from ".$_POST['username']." on ".$_POST['appname']."'s Application! \n Link: ".$member_link;
+$discord_msg = $_POST['appname']."'s Application has been ".$_POST['status']." \n Link: ".$member_link;
 
-notify_guild($member_link,$_POST['appname'],$_POST['username']);
-notify_applicant($app_link,$_POST['username']);
+notify_guild($member_link,$_POST['appname'],$_POST['status']);
+notify_applicant($_POST['status']);
 notify_discord($discord_msg);
 
 $link = 'https://www.stormguild.us'.$_POST['redirecturi'];
 header("Location: $link");
 
-function comment_add() {
+function update_app() {
   $db = new database();
 
   #Clean up and declare all variables
@@ -25,11 +24,7 @@ function comment_add() {
     $$key = $db -> quote($value);
   }
 
-  if (EMPTY($_POST['replyid'])) {
-    $replyid = 'NULL';
-  }
-
-  $com_sql = "INSERT INTO stormguild.app_comment VALUE (NULL,".$replyid.",".$appid.",".$username.",".$message.",now())";
+  $com_sql = "UPDATE stormguild.application SET status = ".$status." WHERE application_id = ".$appid;
 
   $result = $db -> sql_query($com_sql);
   if ($result) {
@@ -51,38 +46,33 @@ function notify_discord($message) {
   return curl_exec($curl);
 }
 
-function notify_guild($link, $appname, $username) {
+function notify_guild($link, $appname, $decision) {
   $msg = new messenger(false);
   global $dbhost, $dbuser, $dbpasswd, $dbname, $accessid;
   $mysqli = new mysqli($dbhost, $dbuser, $dbpasswd, $dbname);
   $result = $mysqli -> query("SELECT username, user_lang, user_email, user_allow_massemail FROM stormforums.bb_users where group_id in (select group_id from stormforums.bb_groups where lower(group_name) in ('officer','raider'))");
   while($row = $result -> fetch_assoc()) {
-    $msg->template('comment_notify_member', '', $_SERVER['DOCUMENT_ROOT'].'/email');
+    $msg->template($decision.'_notify_member', '', $_SERVER['DOCUMENT_ROOT'].'/email');
     $msg->to($row['user_email'], $row['username']);
     $msg->im($row['user_jabber'], $row['username']);
     $msg->from('applications@stormguild.us', 'Storm Raider Applications');
     $msg->assign_vars(array(
         'APP_LINK'  => $link,
-        'APP_NAME' => $appname,
-        'USERNAME' => $username
+        'APP_NAME' => $appname
     ));
     $msg->send($row['user_notify_type']);
   }
 }
 
-function notify_applicant($link, $username) {
+function notify_applicant($decision) {
   $db = new database();
   $msg = new messenger(false);
 
   $result = $db -> sql_fetchrow("SELECT * FROM stormguild.application WHERE application_id = ".$_POST['appid']);
 
-  $msg->template('comment_notify_applicant', '', $_SERVER['DOCUMENT_ROOT'].'/email');
+  $msg->template($decision.'_notify_member', '', $_SERVER['DOCUMENT_ROOT'].'/email');
   $msg->to($result['perEmail'], $result['perName']);
   $msg->from('applications@stormguild.us', 'Storm Raider Applications');
-  $msg->assign_vars(array(
-      'APP_LINK'  => $link,
-      'USERNAME' => $username
-  ));
   $msg->send();
 }
 ?>
