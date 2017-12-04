cdb785edb07b54aa257a16746d4b5ba472e60d39
ismcalpi <ismcalpi@gmail.com>
2017-12-03 21:21:23 -0500

    Add Raids to Admin Console

--- templates/admin.raids.php
+++ templates/admin.raids.php
@@ -49,7 +49,7 @@ $db = new database();
         <thead>
           <tr>
             <th width="20%">Name</th>
-            <th width="40%">Description (Optional)</th>
+            <th width="40%">Description/Notes(Optional)</th>
             <th width="15%">Release Date</th>
             <th width="5%">Active?</th>
             <th width="20%">Action</th>
@@ -94,17 +94,59 @@ $db = new database();
   </div>
 </div>
 
-<!-- Expansions Section -->
+<!-- Raids Section -->
 <div class="row">
-  <div class="col-12"><h2 class="h2 text-upper text-center">Raids</h2></div>
+  <div class="col-12">
+    <h2 class="h2 text-upper text-center">Expansions</h2>
+  </div>
   <div class="col-12 g-pa-20">
     <div class="table-responsive">
       <table class="table text-center">
         <thead>
-
+          <tr>
+            <th width="15%">Name</th>
+            <th width="15%">Expansion</th>
+            <th width="30%">Description/ Notes (Optional)</th>
+            <th width="15%">Release Date</th>
+            <th width="5%">Active?</th>
+            <th width="20%">Action</th>
+          </tr>
         </thead>
         <tbody>
-
+          <tr>
+            <form enctype="multipart/form-data" method="post" action="<?php echo $_SERVER['REQUEST_URI']; ?>" >
+              <input type="hidden" name="table" value="expansion">
+              <td><input name="name" type="text" class="form-control form-control-md rounded-0"></td>
+              <td><input name="name" type="text" class="form-control form-control-md rounded-0"></td>
+              <td><textarea name="decription" class="form-control form-control-md rounded-0" rows="1"></textarea></td>
+              <td><input name="release_date" type="date" class="form-control form-control-md rounded-0"></td>
+              <td><input name="active" class="form-control form-control-md rounded-0" type="number" value="1" readonly=""></td>
+              <td><button type="submit" name="action" value="insert" class="btn btn-md u-btn-blue rounded-0">Add</button></td>
+            </form>
+          </tr>
+        <?php
+          $raids = $db -> sql_select("SELECT r.*, (SELECT name FROM stormguild.expansion e WHERE e.expansion_id = r.expansion_id) as expansion FROM stormguild.raid r ORDER BY release_date DESC");
+          foreach ($raids as $raid) {
+        ?>
+          <tr>
+            <form enctype="multipart/form-data" method="post" action="<?php echo $_SERVER['REQUEST_URI']; ?>" >
+              <input type="hidden" name="id" value="<?php echo $raid['expansion_id'] ?>">
+              <input type="hidden" name="action" value="update">
+              <input type="hidden" name="table" value="expansion">
+              <td><input name="name" type="text" class="form-control form-control-md rounded-0" value="<?php echo $raid['name'] ?>"></td>
+              <td><input name="name" type="text" class="form-control form-control-md rounded-0" value="<?php echo $raid['expansion'] ?>"></td>
+              <td><textarea name="description" class="form-control form-control-md rounded-0" rows="1"><?php echo $raid['description'] ?></textarea></td>
+              <td><input name="release_date" type="date" class="form-control form-control-md rounded-0" value="<?php echo $raid['release_date'] ?>"></td>
+              <td><input name="active" class="form-control form-control-md rounded-0" type="number" value="<?php echo $raid['is_active'] ?>"></td>
+              <td>
+                <button type="submit" name="action" value="update" class="btn btn-md u-btn-blue rounded-0">Update</button>
+                <button type="submit" name="action" value="delete" class="btn btn-md u-btn-blue rounded-0">Delete</button>
+              </td>
+            </form>
+          </tr>
+        <?php
+          }
+        ?>
         </tbody>
       </table>
     </div>
