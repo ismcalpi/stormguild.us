7821fcba77598bacd17b4de356efe9eeda5ced7a
usumcia <imcalpine@scor.com>
2017-12-06 12:55:22 -0500

    Update

--- templates/admin.raids.php
+++ templates/admin.raids.php
@@ -24,14 +24,14 @@ $db = new database();
           <td><strong><a href="#">Test Expansion</a></strong></td>
           <td colspan="5"></td>
         </tr>
-        <tr id="raid1-head" class="collapsed">
+        <tr id="raid1-head" class="collapsed" href="#raid1-body" data-toggle="collapse" data-parent="#raids" aria-expanded="true" aria-controls="raid1-body">
           <td></td>
           <td><strong>
             <span class="u-accordion__control-icon">
               <i class="fa fa-plus"></i>
               <i class="fa fa-minus"></i>
             </span>
-            <a href="#raid1-body" data-toggle="collapse" data-parent="#raids" aria-expanded="true" aria-controls="raid1-body">Antorus the Burning Throne</a>
+            <a href="#">Antorus the Burning Throne</a>
           </strong></td>
           <td><strong>Heroic</strong></td>
           <td><strong>Mythic</strong></td>
