394ca8c0730e5650969a962eb8cf058babf1e072
usumcia <imcalpine@scor.com>
2017-12-06 14:32:38 -0500

    Testing Formatting

--- templates/admin.raids.php
+++ templates/admin.raids.php
@@ -19,43 +19,51 @@ $db = new database();
 
   <div class="container col-12 g-pa-40">
 
-    <a class="h4 text-center text-left g-my-20" href="#">Expansion: Legion</a>
+    <h3 class="h3 text-upper text-left g-my-20">Expansion: Legion</h3>
 
-    <table id="legion" class="table table-hover">
+    <div id="legion" class="card rounded-0">
 
-      <thead>
-        <tr>
-          <th colspan="5">
-            <span id="raid1-head" class="collapsed g-ml-15" href="#raid1-body" data-toggle="collapse" data-parent="#raids" aria-expanded="true" aria-controls="raid1-body">
-              <span class="u-accordion__control-icon g-mr-10">
-                <i class="fa fa-plus"></i>
-                <i class="fa fa-minus"></i>
-              </span>
-              Raid: Antorus, the Burning Throne
-            </span>
-            <button type="button" class="btn btn-sm btn-primary g-ml-10">Edit</button>
-          </th>
-        </tr>
-      </thead>
+      <h3 class="card-header h5 rounded-0">
+        <span id="raid1-head" class="collapsed g-ml-15" href="#raid1-body" data-toggle="collapse" data-parent="#legion" aria-expanded="true" aria-controls="raid1-body">
+          <span class="u-accordion__control-icon g-mr-10">
+            <i class="fa fa-plus"></i>
+            <i class="fa fa-minus"></i>
+          </span>
+          Raid: Antorus, the Burning Throne
+        </span>
+        <button type="button" class="btn btn-sm btn-primary g-ml-10">Edit</button>
+      </h3>
 
-      <tbody id="raid1-body" aria-labelledby="raid1-head" class="collapse">
-        <tr>
-          <th><strong>Boss Name</strong></th>
-          <th><strong>Heroic</strong></th>
-          <th><strong>Mythic</strong></th>
-          <th><strong>Killshot</strong></th>
-          <th><strong>Video</strong></th>
-        </tr>
-        <tr >
-          <td><a href="#">Garothi Worldbreaker</a></td>
-          <td>12/5/2017</td>
-          <td>12/5/2017</td>
-          <td>/path/to/image.png</td>
-          <td>https://twitch.com/</td>
-        </tr>
-      </tbody>
+      <div class="card-block">
+        <table class="table table-hover">
+          <thead>
+            <tr>
+              <th><strong>Boss Name</strong></th>
+              <th><strong>Heroic</strong></th>
+              <th><strong>Mythic</strong></th>
+              <th><strong>Killshot</strong></th>
+              <th><strong>Video</strong></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td><a href="#">Garothi Worldbreaker</a></td>
+              <td>12/5/2017</td>
+              <td>12/5/2017</td>
+              <td>/path/to/image.png</td>
+              <td>https://twitch.com/</td>
+            </tr>
+            <tr>
+              <td><a href="#">Garothi Worldbreaker</a></td>
+              <td>12/5/2017</td>
+              <td>12/5/2017</td>
+              <td>/path/to/image.png</td>
+              <td>https://twitch.com/</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
 
-    </table>
-
+    </div>
   </div>
 </div>
