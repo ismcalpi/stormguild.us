c820a1fbcaea375819f52600c38f88b96ab3dfcd
usumcia <imcalpine@scor.com>
2017-12-06 12:41:37 -0500

    test dropdown

--- templates/admin.raids.php
+++ templates/admin.raids.php
@@ -16,22 +16,22 @@ $db = new database();
   <div class="col-12">
     <h2 class="h2 text-center text-upper">Raid Progression</h2>
   </div>
-  <div class="table-responsive container col-12 g-pa-25">
+  <div id="raids" class="table-responsive container col-12 g-pa-25">
     <table class="table table-hover">
       <tbody>
         <tr>
           <td><strong><a href="#">Test Expansion</a></strong></td>
           <td colspan="5"></td>
         </tr>
-        <tr>
+        <tr id="raid1-head">
           <td></td>
-          <td><strong><a href="#">Test Raid</a></strong></td>
+          <td><strong><a href="#raid1-body" data-toggle="collapse" data-parent="#raids" aria-expanded="true" aria-controls="raid1-body">Test Raid</a></strong></td>
           <td><strong>Heroic</strong></td>
           <td><strong>Mythic</strong></td>
           <td><strong>Killshot</strong></td>
           <td><strong>Video</strong></td>
         </tr>
-        <tr>
+        <tr id="raid1-body" aria-labelledby="raid1-head">
           <td></td>
           <td><a href="#">Test Boss 1</a></td>
           <td>Test Date</td>
