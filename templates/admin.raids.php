2f7e24a8d7ac880a95c7244d96907a69c2e8af41
usumcia <imcalpine@scor.com>
2017-12-06 13:22:11 -0500

    Fix Formatting

--- templates/admin.raids.php
+++ templates/admin.raids.php
@@ -23,7 +23,7 @@ $db = new database();
           <td colspan="5" class="text-upper text-left"><strong><a href="#">Legion</a></strong></td>
         </tr>
         <tr id="raid1-head" class="collapsed g-ml-15" href="#raid1-body" data-toggle="collapse" data-parent="#raids" aria-expanded="true" aria-controls="raid1-body">
-          <td class="text-upper">
+          <td class="text-upper g-ml-10">
             <strong>
             <span class="u-accordion__control-icon g-mr-10">
               <i class="fa fa-plus"></i>
@@ -37,8 +37,8 @@ $db = new database();
           <td><strong>Killshot</strong></td>
           <td><strong>Video</strong></td>
         </tr>
-        <tr id="raid1-body" aria-labelledby="raid1-head" class="collapse g-ml-15">
-          <td><a href="#">Garothi Worldbreaker</a></td>
+        <tr id="raid1-body" aria-labelledby="raid1-head" class="collapse">
+          <td class="g-ml-20"><a href="#">Garothi Worldbreaker</a></td>
           <td>12/5/2017</td>
           <td>12/5/2017</td>
           <td>/path/to/image.png</td>
