90737e2369932e7cc44eef29ae40ae61cf4d6fb8
usumcia <imcalpine@scor.com>
2017-12-06 13:20:17 -0500

    Update

--- templates/admin.raids.php
+++ templates/admin.raids.php
@@ -18,37 +18,26 @@ $db = new database();
   </div>
   <div id="raids" class="u-accordion container col-12 g-pa-25" role="tablist" aria-multiselectable="true">
     <table class="table table-hover">
-      <thead>
-        <tr>
-          <th />
-          <th width="20%" />
-          <th width="15%" />
-          <th width="15%" />
-          <th width="20%" />
-          <th width="20%" />
-        </tr>
-      </thead>
       <tbody>
         <tr>
-          <td class="text-upper"><strong><a href="#">Legion</a></strong></td>
-          <td colspan="5"></td>
+          <td colspan="5" class="text-upper text-left"><strong><a href="#">Legion</a></strong></td>
         </tr>
-        <tr id="raid1-head" class="collapsed" href="#raid1-body" data-toggle="collapse" data-parent="#raids" aria-expanded="true" aria-controls="raid1-body">
-          <td></td>
-          <td class="text-upper"><strong>
-            <span class="u-accordion__control-icon p-mr-15">
+        <tr id="raid1-head" class="collapsed g-ml-15" href="#raid1-body" data-toggle="collapse" data-parent="#raids" aria-expanded="true" aria-controls="raid1-body">
+          <td class="text-upper">
+            <strong>
+            <span class="u-accordion__control-icon g-mr-10">
               <i class="fa fa-plus"></i>
               <i class="fa fa-minus"></i>
             </span>
             <a href="#">Antorus the Burning Throne</a>
-          </strong></td>
+            </strong>
+          </td>
           <td><strong>Heroic</strong></td>
           <td><strong>Mythic</strong></td>
           <td><strong>Killshot</strong></td>
           <td><strong>Video</strong></td>
         </tr>
-        <tr id="raid1-body" aria-labelledby="raid1-head" class="collapse">
-          <td></td>
+        <tr id="raid1-body" aria-labelledby="raid1-head" class="collapse g-ml-15">
           <td><a href="#">Garothi Worldbreaker</a></td>
           <td>12/5/2017</td>
           <td>12/5/2017</td>
