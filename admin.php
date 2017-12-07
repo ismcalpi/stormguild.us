98f973880278b572d6362ebeaf5d90612778845f
ismcalpi <ismcalpi@gmail.com>
2017-12-07 08:01:15 -0500

    Update

--- admin.php
+++ admin.php
@@ -24,12 +24,12 @@
         <?php
         } else {
         ?>
-				<form id="redirect" method="POST" action="user.php?page=login">
-					<input type="hidden" name="redirect" value="<?php echo $_SERVER['REQUEST_URI'] ?>">
-				</form>
-				<script type="text/javascript">
-						document.getElementById('redirect').submit(); // SUBMIT FORM
-				</script>
+					<form id="redirect" method="POST" action="user.php?page=login">
+						<input type="hidden" name="redirect" value="<?php echo $_SERVER['REQUEST_URI'] ?>">
+					</form>
+					<script type="text/javascript">
+							document.getElementById('redirect').submit(); // SUBMIT FORM
+					</script>
         <?php
         }
         ?>
