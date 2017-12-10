68ca0650d0493fd3a6e539541b25bd512d834040
ismcalpi <ismcalpi@gmail.com>
2017-12-10 15:17:43 -0500

    Application updates
    * Needed to spread the questions and use htmlspecialchar function on answers to avoid losing some characters.

--- templates/application.body.php
+++ templates/application.body.php
@@ -3,6 +3,11 @@ include_once 'library/class.database.php';
 $db = new database();
 $application = $db -> sql_fetchrow("SELECT * FROM stormguild.application WHERE application_id =".$appid);
 if(!empty($application)) {
+
+  foreach($application as $key => $value){
+    $$key = htmlspecialchars($value);
+  }
+
 ?>
 <!-- Screening Questions -->
 <div class="row">
@@ -32,25 +37,25 @@ if(!empty($application)) {
     <div class="col-sm-2">
         <p>
             <strong>Name:</strong>
-            <?php echo $application['perName']; ?>
+            <?php echo $perName; ?>
         </p>
     </div>
     <div class="col-sm-2">
         <p>
             <strong>Age:</strong>
-            <?php echo $application['perAge']; ?>
+            <?php echo $perAge; ?>
         </p>
     </div>
     <div class="col-sm-4">
         <p>
             <strong>E-Mail:</strong>
-            <?php echo $application['perEmail']; ?>
+            <?php echo $perEmail; ?>
         </p>
     </div>
     <div class="col-sm-4">
         <p>
             <strong>Battle ID:</strong>
-            <?php echo $application['perBnet']; ?>
+            <?php echo $perBnet; ?>
         </p>
     </div>
 </div>
@@ -66,44 +71,44 @@ if(!empty($application)) {
         <div class="row">
             <div class="col-sm-8">
                 <h5>
-                    <?php echo $application['charName']; ?>
+                    <?php echo $charName; ?>
                     -
-                    <?php echo $application['charRealm']; ?>
-                    (<?php echo $application['charSpec'].' '.$application['charClass']; ?>)
+                    <?php echo $charRealm; ?>
+                    (<?php echo $charSpec.' '.$charClass; ?>)
                     <br/>
                     <small>
-                        <?php echo $application['charArt']; ?>
+                        <?php echo $charArt; ?>
                     </small>
                 </h5>
             </div>
             <div class="col-sm-2 g-py-5">
-                <a target="_blank" href="<?php echo $application['charArmory']; ?>" class="btn btn-block u-btn-outline-blue g-mb-10">Armory</a>
+                <a target="_blank" href="<?php echo $charArmory; ?>" class="btn btn-block u-btn-outline-blue g-mb-10">Armory</a>
             </div>
             <div class="col-sm-2 g-py-5">
-                <a target="_blank" href="<?php echo $application['charLogs']; ?>" class="btn btn-block u-btn-outline-purple g-mb-10">Logs</a>
+                <a target="_blank" href="<?php echo $charLogs; ?>" class="btn btn-block u-btn-outline-purple g-mb-10">Logs</a>
             </div>
         </div>
     </div>
     <div class="col-sm-12 g-my-10">
         <!-- Alt Character -->
-        <div class="row <?php if(empty($application['altName'])) {echo "g-hide"; } ?>">
+        <div class="row <?php if(empty($altName)) {echo "g-hide"; } ?>">
             <div class="col-8">
                 <h5>
-                    <?php echo $application['altName']; ?>
+                    <?php echo $altName; ?>
                     -
-                    <?php echo $application['altRealm']; ?>
-                    (<?php echo $application['altSpec'].' '.$application['altClass']; ?>)
+                    <?php echo $altRealm; ?>
+                    (<?php echo $altSpec.' '.$altClass; ?>)
                     <br/>
                     <small>
-                        <?php echo $application['altArt']; ?>
+                        <?php echo $altArt; ?>
                     </small>
                 </h5>
             </div>
             <div class="col-2 g-py-5">
-                <a target="_blank" href="<?php echo $application['altArmory']; ?>" class="btn btn-block u-btn-outline-blue g-mb-10">Armory</a>
+                <a target="_blank" href="<?php echo $altArmory; ?>" class="btn btn-block u-btn-outline-blue g-mb-10">Armory</a>
             </div>
             <div class="col-2 g-py-5">
-                <a target="_blank" href="<?php echo $application['altLogs']; ?>" class="btn btn-block u-btn-outline-purple g-mb-10">Logs</a>
+                <a target="_blank" href="<?php echo $altLogs; ?>" class="btn btn-block u-btn-outline-purple g-mb-10">Logs</a>
             </div>
         </div>
     </div>
@@ -116,51 +121,45 @@ if(!empty($application)) {
         </div>
     </div>
     <div class="col-sm-12">
-        <p><strong>
+        <p class="g-mb-20"><strong>
             Are you comfortable playing your off-spec in a raid environment? Elaborate.
-        </strong></p>
-        <p>
-            <?php echo nl2br($application['quest01']); ?>
+        </strong><br />
+            <?php echo nl2br($quest01); ?>
         </p>
     </div>
     <div class="col-sm-12">
-        <p><strong>
+        <p class="g-mb-20"><strong>
             What is your raiding experience? Both current and past progression.
-        </strong></p>
-        <p>
-            <?php echo nl2br($application['quest02']); ?>
+        </strong><br />
+            <?php echo nl2br($quest02); ?>
         </p>
     </div>
     <div class="col-sm-12">
-        <p><strong>
+        <p class="g-mb-20"><strong>
             Previous guilds? Why did you leave them and if applicable why are you leaving them for us?
-        </strong></p>
-        <p>
-            <?php echo nl2br($application['quest03']); ?>
+        </strong><br />
+            <?php echo nl2br($quest03); ?>
         </p>
     </div>
     <div class="col-sm-12">
-        <p><strong>
+        <p class="g-mb-20"><strong>
             How did you hear about us and why would you like to join? Why would you be an asset to our roster?
-        </strong></p>
-        <p>
-            <?php echo nl2br($application['quest04']); ?>
+        </strong><br />
+            <?php echo nl2br($quest04); ?>
         </p>
     </div>
     <div class="col-sm-12">
-        <p><strong>
+        <p class="g-mb-20"><strong>
             How do you prepare for a new progression boss? Would you consider dps/healing output or mechanics more important?
-        </strong></p>
-        <p>
-            <?php echo nl2br($application['quest05']); ?>
+        </strong><br />
+            <?php echo nl2br($quest05); ?>
         </p>
     </div>
     <div class="col-sm-12">
-        <p><strong>
+        <p class="g-mb-20"><strong>
             Please post a screenshot of your UI (in raid if possible) and explain any add-ons you consider essential for yourself.
-        </strong></p>
-        <p>
-            <?php echo nl2br($application['quest06']); ?>
+        </strong><br />
+            <?php echo nl2br($quest06); ?>
         </p>
         <a style="max-height:200px;max-width:300px;"
            class="js-fancybox d-block u-block-hover g-my-20"
@@ -174,19 +173,17 @@ if(!empty($application)) {
         </a>
     </div>
     <div class="col-sm-12">
-        <p><strong>
+        <p class="g-mb-20"><strong>
             Are you comfortable speaking on Discord? If you refuse to speak don't say yes. What kind of mic/headset do you use?
-        </strong></p>
-        <p>
-            <?php echo nl2br($application['quest07']); ?>
+        </strong><br />
+            <?php echo nl2br($quest07); ?>
         </p>
     </div>
     <div class="col-sm-12">
-        <p><strong>
+        <p class="g-mb-20"><strong>
             Anything else you'd like us to know? Tell us a bit about yourself.
-        </strong></p>
-        <p>
-            <?php echo nl2br($application['quest08']); ?>
+        </strong><br />
+            <?php echo nl2br($quest08); ?>
         </p>
     </div>
 </div>
