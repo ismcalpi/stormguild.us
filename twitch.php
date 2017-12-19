164c2fbdb281a9cd9437dd570754ad62c7c1fb98
usumcia <imcalpine@scor.com>
2017-12-19 13:20:02 -0500

    Fix

--- twitch.php
+++ twitch.php
@@ -10,11 +10,11 @@
 				<?php
 
 					$curl = curl_init();
-					curl_setopt($curl, CURLOPT_URL, 'https://api.twitch.tv/kraken/users?login=kniny');
-					curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-					curl_setopt($curl, CURLOPT_HEADER, 'Accept: application/vnd.twitchtv.v5+json');
-					curl_setopt($curl, CURLOPT_HEADER, 'Client-ID: dixpnolwj0yth0r3wpzxrp2edowugp');
-
+					curl_setopt_array($curl, array(
+					    CURLOPT_RETURNTRANSFER => 1,
+					    CURLOPT_URL => 'https://api.twitch.tv/kraken/users?login=kniny',
+					    CURLOPT_HTTPHEADER => array('Accept: application/vnd.twitchtv.v5+json','Client-ID: dixpnolwj0yth0r3wpzxrp2edowugp')
+					));
 					// Find User Information
 					$twitch_user = curl_exec($curl);
 					$json_user = json_decode(file_get_contents($twitch_user));
@@ -30,7 +30,6 @@
 			      <div class="g-mt-minus-20">
 			        <h4 class="h6 g-color-primary g-font-weight-600 text-uppercase g-mb-5"><?php echo $json_user->display_name; ?></h4>
 			        <em class="d-block g-color-gray-dark-v4 g-font-style-normal g-font-size-13 g-mb-20">Shadow Priest</em>
-			        <i class="d-block g-color-primary g-font-size-50 g-line-height-0_7 g-pos-rel g-top-15 g-mb-20">“</i>
 			        <blockquote class="g-color-black g-font-style-italic g-font-size-20 g-line-height-1_4">Does stuff with tentacles.</blockquote>
 			      </div>
 			    </div>
