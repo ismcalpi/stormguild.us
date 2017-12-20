45fe94ae51aacae617e643aa65e64b82196aedad
usumcia <imcalpine@scor.com>
2017-12-20 12:51:44 -0500

    Update

--- templates/home.progression.php
+++ templates/home.progression.php
@@ -3,7 +3,7 @@ $json = json_decode(file_get_contents('https://www.wowprogress.com/guild/us/stor
 ?>
 <div class="col-12 g-pa-0 g-ma-0">
 	<a  href="https://www.wowprogress.com/guild/us/stormrage/storm"
-			class="btn btn-xl u-btn-bluegray u-btn-content g-font-weight-600 g-letter-spacing-0_5 text-uppercase g-brd-2 g-my-5 g-pa-5"
+			class="btn btn-xl u-btn-bluegray u-btn-content g-font-weight-600 g-letter-spacing-0_5 text-uppercase g-brd-2 g-mt-5 g-pa-5"
 			target="_blank"
 			style="width:100%;">
 		<i class="fa fa-globe pull-left g-font-size-35"></i>
@@ -14,7 +14,7 @@ $json = json_decode(file_get_contents('https://www.wowprogress.com/guild/us/stor
 	</a>
 </div>
 
-<div class="col-12 g-pa-0 g-ma-0">
+<div class="col-12 g-pa-0 g-my-0">
 
 <?php
 include_once 'library/class.database.php';
@@ -26,7 +26,7 @@ foreach($raids as $raid) {
 ?>
 
 	<a  href=""
-			class="btn btn-xl u-btn-content g-font-weight-600 g-letter-spacing-0_5 text-uppercase g-my-5 g-pa-5"
+			class="btn btn-xl u-btn-content g-font-weight-600 g-letter-spacing-0_5 text-uppercase g-mt-5 g-pa-5"
 			style="width:100%;background-image:url('img/raid/antorus/main.jpg');background-size:cover;">
 		<span class="u-accordion__control-icon d-inline-block g-color-white pull-left">
 			<i class="fa fa-plus"></i>
