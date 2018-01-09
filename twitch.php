b173fee4ad5fb5fc974b6fa5518d16504adc9c8a
usumcia <imcalpine@scor.com>
2018-01-09 12:39:26 -0500

    Format Fix

--- twitch.php
+++ twitch.php
@@ -23,7 +23,7 @@
 							$channel = getChannelJSON($result['username']);
 					?>
 
-					<div class="u-shadow-v19 col-md-3 col-6 g-bg-white g-brd-around g-brd-purple g-brd-2 text-center rounded g-pb-20 g-px-15 g-my-50 g-mx-10">
+					<div class="u-shadow-v19 col-md-4 col-6 g-bg-white g-brd-around g-brd-purple g-brd-2 text-center rounded g-pb-20 g-px-15 g-my-50 g-mx-10">
 			      <img class="g-brd-7 g-brd-style-solid g-brd-white g-width-100 g-height-100 rounded-circle g-pull-50x-up" src="<?php echo $user['users'][0]['logo']; ?>" alt="Image Description">
 			      <div class="g-mt-minus-20">
 			        <h4 class="h6 g-color-purple g-font-weight-600 text-uppercase g-mb-5"><?php echo $user['users'][0]['display_name']; ?></h4>
