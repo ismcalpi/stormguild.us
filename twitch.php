9dd83c622927a5a51d2ab2ab7e08aaf6b9738464
usumcia <imcalpine@scor.com>
2018-01-09 12:38:52 -0500

    Update

--- twitch.php
+++ twitch.php
@@ -22,8 +22,8 @@
 							$user = getUserJSON($result['username']);
 							$channel = getChannelJSON($result['username']);
 					?>
-					<!-- Blah Twitch Stuff -->
-					<div class="u-shadow-v19 col-md-3 col-sm-6 g-bg-white g-brd-around g-brd-purple g-brd-2 text-center rounded g-pb-40 g-px-30 g-my-50 g-mx-20">
+
+					<div class="u-shadow-v19 col-md-3 col-6 g-bg-white g-brd-around g-brd-purple g-brd-2 text-center rounded g-pb-20 g-px-15 g-my-50 g-mx-10">
 			      <img class="g-brd-7 g-brd-style-solid g-brd-white g-width-100 g-height-100 rounded-circle g-pull-50x-up" src="<?php echo $user['users'][0]['logo']; ?>" alt="Image Description">
 			      <div class="g-mt-minus-20">
 			        <h4 class="h6 g-color-purple g-font-weight-600 text-uppercase g-mb-5"><?php echo $user['users'][0]['display_name']; ?></h4>
@@ -35,7 +35,7 @@
 							<?php }?>
 			      </div>
 			    </div>
-					<!-- End Blah Section -->
+
 				<?php } ?>
 				</div>
 			</div>
