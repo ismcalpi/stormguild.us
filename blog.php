40d9235c1284553bcc33fb1f5424c3698fb0f810
ismcalpi <ismcalpi@gmail.com>
2018-01-21 12:04:36 -0500

    Try to make iframe load quicker

--- blog.php
+++ blog.php
@@ -2,7 +2,7 @@
   <?php include 'templates/home.head.php' ?>
   <body>
     <main style="background:rgba(255,255,255,0.01);">
-      <div class="container g-py-10 p-px-5">
+      <div class="container g-py-10 p-px-5 g-brd-around g-brd-gray-light-v4">
         <div class="row justify-content-center">
           <div class="col-12">
       <?php
@@ -15,9 +15,7 @@
       ?>
       <article class="g-mb-15">
         <div class="g-mb-30">
-          <!-- <a class="js-fancybox" href="<?php the_post_thumbnail_url(); ?>" data-fancybox-animate-in="zoomIn" data-fancybox-animate-out="zoomOut" data-fancybox-speed="1000" data-fancybox-bg="rgba(0,0,0, 1)"> -->
-            <img class="img-fluid w-100 g-mb-25" src="<?php the_post_thumbnail_url(); ?>" alt="Image Description">
-          <!-- </a> -->
+          <a target="_blank" href="<?php the_post_thumbnail_url(); ?>"><img class="img-fluid w-100 g-mb-25" src="<?php the_post_thumbnail_url(); ?>" alt="Image Description"></a>
           <h2 class="h4 g-color-black g-font-weight-600 mb-3">
             <a target=_blank class="u-link-v5 g-color-black g-color-primary--hover" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
             <span class="d-block g-color-gray-dark-v4 g-font-size-12"><?php the_time('l jS F, Y') ?></span>
@@ -26,8 +24,6 @@
           <a target=_blank class="g-font-size-13" href="<?php the_permalink(); ?>">Read more...</a>
         </div>
       </article>
-
-      <hr class="u-divider-dashed g-brd-gray-light-v2 g-my-15">
 
       <?php }
         } else {
@@ -39,6 +35,5 @@
         </div>
       </div>
     </main>
-  <?php include 'templates/home.js.php' ?>
   </body>
 </html>
