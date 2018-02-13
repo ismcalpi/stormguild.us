52df854f013b050935abba28bbb54fc4e46c9fee
usumcia <imcalpine@scor.com>
2018-02-13 13:36:41 -0500

    Move banners to DB

--- templates/home.banner.php
+++ templates/home.banner.php
@@ -1,10 +1,15 @@
 <div class="master-slider g-ma-0 g-pa-0 ms-skin-default" id="masterslider">
+  <?php
+  include_once 'library/class.database.php';
+  $db = new database();
+  $banners = $db -> read_select("SELECT * FROM stormguild.banners WHERE is_active = 1");
+  foreach($banners as $banner) {
+  ?>
   <div class="ms-slide g-ma-0 g-pa-0" data-delay="10">
-    <img style="max-width:100%;max-height:100%;" src="assets\img\uploads\banner\recruitment.png" data-src="assets\img\uploads\banner\recruitment.png" />
-    <a href="recruit.php"></a>
+    <img style="max-width:100%;max-height:100%;" src="<?php echo $banner['path']; ?>" data-src="<?php echo $banner['path']; ?>" />
+    <a href="<?php echo $banner['url']; ?>"></a>
   </div>
-  <div class="ms-slide g-ma-0 g-pa-0" data-delay="10">
-    <img style="max-width:100%;max-height:100%;" src="assets\img\uploads\banner\sales.png" data-src="assets\img\uploads\banner\sales.png" />
-    <a href="sales.php"></a>
-  </div>
+  <?php
+  }
+  ?>
 </div>
