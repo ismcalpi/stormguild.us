7d737dec7a52d6a92ff6e0376ae0adb8d334ece0
ismcalpi <ismcalpi@gmail.com>
2018-04-02 11:00:09 -0400

    Add Roster to Live

--- library/http.update.members.php
+++ library/http.update.members.php
@@ -5,17 +5,43 @@
   $db = new database();
 
   $json = json_decode(file_get_contents('https://us.api.battle.net/wow/guild/Stormrage/Storm?fields=members&locale=en_US&apikey=mqa6xqp4dmuvbh4v6s3vu6xjgg75sryb'));
-  foreach($json->members as $obj) {
-    $playerName = $obj->character->name;
-    $playerSpec = $obj->character->spec->icon;
-    $playerClass = $obj->character->class;
-    $playerThumb = $obj->character->thumbnail;
-    $playerRank = $obj->rank;
 
+  foreach ($json->members as $member) {
+    $playerName = $member->character->name;
+    $playerClass = $member->character->class;
+    $playerRank = $member->rank;
+    $playerThumb = $member->character->thumbnail;
 
+    if(!$member->character->spec) {
+      $playerSpec = "NULL";
+    } else {
+      $playerSpec = $member->character->spec->icon;
+    }
 
+    $sqlName = $db -> quote($playerName);
+    $sqlClass = $db -> quote($playerClass);
+    $sqlRank = $db -> quote($playerRank);
+    $sqlThumb = $db -> quote($playerThumb);
+    $sqlSpec = $db -> quote($playerSpec);
+
+    $sql = "SELECT * FROM stormguild.guild_roster WHERE name = {$sqlName} AND class = {$sqlClass}";
+    $player = $db -> read_row($sql);
+    if (!$player) {
+      $sql = "INSERT INTO stormguild.guild_roster VALUES (NULL,{$sqlName},{$sqlRank},{$sqlSpec},{$sqlClass},{$sqlThumb},now(),NULL)";
+      $result = $db -> write_query($sql);
+    } else if ($player['rank'] != $playerRank) {
+      $sql = "UPDATE stormguild.guild_roster SET rank = {$sqlRank} WHERE member_id = {$player['member_id']}";
+      $result = $db -> write_query($sql);
+    } else if ($player['spec_icon'] != $playerSpec) {
+      $sql = "UPDATE stormguild.guild_roster SET spec_icon = {$sqlSpec} WHERE member_id = {$player['member_id']}";
+      $result = $db -> write_query($sql);
+    } else if ($player['thumbnail'] != $playerThumb) {
+      $sql = "UPDATE stormguild.guild_roster SET thumbnail = {$sqlThumb} WHERE member_id = {$player['member_id']}";
+      $result = $db -> write_query($sql);
+    } else {
+
+    }
+
+    echo 'Handling '.$playerName.' now. Class: '.$playerClass.' Spec: '.$playerSpec.' Rank: '.$playerRank.' Thumbnail: '.$playerThumb.' <br />';
   }
-
-  https://wow.zamimg.com/images/wow/icons/large/$spec_img.jpg
-  http://render-api-us.worldofwarcraft.com/static-render/us/$char_url
 ?>
--- templates/all.navbar.php
+++ templates/all.navbar.php
@@ -73,9 +73,24 @@
                 <img class="u-block-hover__img u-block-hover__main--blur" style="max-height:40px;" src="assets/img/logo.storm.png" title="Home">
               </a>
             </li>
-            <li class="nav-item g-mx-20--lg g-mb-5 g-mb-0--lg <?php $aboutActive ?>">
-              <a href="./about.php" class="nav-link">About Us</a>
+            <li class="nav-item dropdown g-mx-20--lg">
+              <a href="#" class="nav-link dropdown-toggle" aria-haspopup="true" aria-expanded="false" aria-controls="nav-submenu-1" data-toggle="dropdown">Guild Info</a>
+              <!-- Submenu -->
+              <ul class="dropdown-menu rounded-0 g-text-transform-none g-brd-none g-brd-top g-brd-bottom g-brd-bottom-2 g-brd-blue g-brd-top-2 g-mt-0 g-mt-10--lg--scrolling" aria-labelledby="nav-link-1">
+                <li class="dropdown-item">
+                  <a class="nav-link g-px-0" href="about.php">About Us</a>
+                </li>
+                <li class="dropdown-item">
+                  <a class="nav-link g-px-0" href="roster.php">Guild Roster</a>
+                </li>
+                <li class="dropdown-item">
+                  <a class="nav-link g-px-0" href="sales.php">Sales</a>
+                </li>
+              </ul>
             </li>
+            <!-- <li class="nav-item g-mx-20--lg g-mb-5 g-mb-0--lg <?php $aboutActive ?>">
+              <a href="./about.php" class="nav-link">About Us</a>
+            </li> -->
             <!-- <li class="nav-item g-mx-20--lg g-mb-5 g-mb-0--lg <?php $salesActive ?>">
               <a href="./sales.php" class="nav-link">Sales</a>
             </li> -->
@@ -88,7 +103,7 @@
             <li class="nav-item dropdown g-mx-20--lg">
               <a href="#" class="nav-link dropdown-toggle g-color-blue" id="nav-link-1" aria-haspopup="true" aria-expanded="false" aria-controls="nav-submenu-1" data-toggle="dropdown"><?php echo $phpbb_username; ?></a>
               <!-- Submenu -->
-              <ul class="dropdown-menu rounded-0 g-text-transform-none g-brd-none g-brd-top g-brd-blue g-brd-top-2 g-mt-20 g-mt-10--lg--scrolling" id="nav-submenu-1" aria-labelledby="nav-link-1">
+              <ul class="dropdown-menu rounded-0 g-text-transform-none g-brd-none g-brd-top g-brd-bottom g-brd-bottom-2 g-brd-blue g-brd-top-2 g-mt-0 g-mt-10--lg--scrolling" id="nav-submenu-1" aria-labelledby="nav-link-1">
                 <?php
                   if($user_rank >= 3) {
                 ?>
