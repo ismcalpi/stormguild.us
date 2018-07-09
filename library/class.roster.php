f4adb3bba05f6a357d30d440ac712e6c0f841c16
usumcia <imcalpine@scor.com>
2018-07-09 13:18:56 -0400

    Add Roster V2

---
+++ library/class.roster.php
@@ -0,0 +1,113 @@
+<?php
+	class roster {
+
+    include_once 'library/class.database.php';
+    $db = new Database();
+
+    public function get_classInfo ($classNum) {
+      switch ($classNum) {
+        case 1:
+          $className = 'Warrior';
+          $classColor = '#C79C6E';
+          $classIcon = 'http://wow.zamimg.com/images/classes/symbols/icon-warrior.png'
+          break;
+        case 2:
+          $className = 'Paladin';
+          $classColor = '#F58CBA';
+          $classIcon = 'http://wow.zamimg.com/images/classes/symbols/icon-paladin.png'
+          break;
+        case 3:
+          $className = 'Hunter';
+          $classColor = '#ABD473';
+          $classIcon = 'http://wow.zamimg.com/images/classes/symbols/icon-hunter.png'
+          break;
+        case 4:
+          $className = 'Rogue';
+          $classColor = '#FFF569';
+          $classIcon = 'http://wow.zamimg.com/images/classes/symbols/icon-rogue.png'
+          break;
+        case 5:
+          $className = 'Priest';
+          $classColor = '#FFFFFF';
+          $classIcon = 'http://wow.zamimg.com/images/classes/symbols/icon-priest.png'
+          break;
+        case 6:
+          $className = 'Death Knight';
+          $classColor = '#C41F3B';
+          $classIcon = 'http://wow.zamimg.com/images/classes/symbols/icon-deathknight.png'
+          break;
+        case 7:
+          $className = 'Shaman';
+          $classColor = '#0070DE';
+          $classIcon = 'http://wow.zamimg.com/images/classes/symbols/icon-shaman.png'
+          break;
+        case 8:
+          $className = 'Mage';
+          $classColor = '#69CCF0';
+          $classIcon = 'http://wow.zamimg.com/images/classes/symbols/icon-mage.png'
+          break;
+        case 9:
+          $className = 'Warlock';
+          $classColor = '#9482C9';
+          $classIcon = 'http://wow.zamimg.com/images/classes/symbols/icon-warlock.png'
+          break;
+        case 10:
+          $className = 'Monk';
+          $classColor = '#00FF96';
+          $classIcon = 'http://wow.zamimg.com/images/classes/symbols/icon-monk.png'
+          break;
+        case 11:
+          $className = 'Druid';
+          $classColor = '#FF7D0A';
+          $classIcon = 'http://wow.zamimg.com/images/classes/symbols/icon-druid.png'
+          break;
+        case 12:
+          $className = 'Demon Hunter';
+          $classColor = '#A330C9';
+          $classIcon = 'http://wow.zamimg.com/images/classes/symbols/icon-demonhunter.png'
+          break;
+        default:
+          break;
+      }
+
+      $classInfo = array($className, $classColor, $classIcon);
+      return $classInfo;
+
+    }
+
+    public function get_guildRank ($rankNum) {
+      switch ($rankNum) {
+        case 0:
+          $rankName = 'Guild Master';
+          break;
+        case 2:
+          $rankName = 'Officer';
+          break;
+        case 4:
+          $rankName = 'Veteran Raider';
+          break;
+        case 6:
+          $rankName = 'Raider';
+          break;
+      }
+      return $rankName;
+    }
+
+    public function get_classResult ($classNum) {
+      return $db -> read_select("SELECT * FROM stormguild.guild_roster WHERE rank in (0,2,4,6) AND class = $classNum order by class asc, rank asc, name asc";
+    }
+
+    public function get_classList () {
+      $results = $db -> read_select("SELECT DISTINCT class from stormguild.guild_roster WHERE rank in (0,2,4,6) order by class asc");
+      $classList = array();
+      foreach ($results as $result) {
+        $classInfo = get_classInfo($result['class']);
+        array_push($classList,$classinfo);
+      }
+      sort($classList);
+      return $classList;
+    }
+
+	}
+
+?>
---
+++ roster2.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+  <?php include 'templates/all.user.php' ?>
+  <?php include 'templates/home.head.php' ?>
+  <body class="main-body">
+    <main>
+      <?php include 'templates/all.navbar.php' ?>
+      <div class="container g-bg-white g-brd-around g-brd-white g-brd-2 g-mt-80 g-pa-30" style="min-height:100vh;">
+        <div class="u-heading-v3-1 text-center g-mb-15">
+          <h2 class="text-uppercase h4 u-heading-v3__title g-brd-blue">Guild Roster V2.0 Testing</h2>
+        </div>
+        <div class="row">
+          <?php
+
+            include_once 'library/class.roster.php';
+            $roster = new Roster();
+
+            $classList = $roster -> get_classList();
+            foreach ($classList as $class) {
+
+              echo "<p>$class[0], $class[1], $class[2]</p>";
+
+            }
+          ?>
+
+        </div>
+      </div>
+    </main>
+  </body>
+  <?php include 'templates/home.js.php' ?>
+</html>
