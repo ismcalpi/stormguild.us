78667ab6fa1b5f8efd02f0751c7eee4376905237
usumcia <imcalpine@scor.com>
2018-07-09 13:21:55 -0400

    Fix

--- library/class.roster.php
+++ library/class.roster.php
@@ -6,62 +6,62 @@
         case 1:
           $className = 'Warrior';
           $classColor = '#C79C6E';
-          $classIcon = 'http://wow.zamimg.com/images/classes/symbols/icon-warrior.png'
+          $classIcon = 'http://wow.zamimg.com/images/classes/symbols/icon-warrior.png';
           break;
         case 2:
           $className = 'Paladin';
           $classColor = '#F58CBA';
-          $classIcon = 'http://wow.zamimg.com/images/classes/symbols/icon-paladin.png'
+          $classIcon = 'http://wow.zamimg.com/images/classes/symbols/icon-paladin.png';
           break;
         case 3:
           $className = 'Hunter';
           $classColor = '#ABD473';
-          $classIcon = 'http://wow.zamimg.com/images/classes/symbols/icon-hunter.png'
+          $classIcon = 'http://wow.zamimg.com/images/classes/symbols/icon-hunter.png';
           break;
         case 4:
           $className = 'Rogue';
           $classColor = '#FFF569';
-          $classIcon = 'http://wow.zamimg.com/images/classes/symbols/icon-rogue.png'
+          $classIcon = 'http://wow.zamimg.com/images/classes/symbols/icon-rogue.png';
           break;
         case 5:
           $className = 'Priest';
           $classColor = '#FFFFFF';
-          $classIcon = 'http://wow.zamimg.com/images/classes/symbols/icon-priest.png'
+          $classIcon = 'http://wow.zamimg.com/images/classes/symbols/icon-priest.png';
           break;
         case 6:
           $className = 'Death Knight';
           $classColor = '#C41F3B';
-          $classIcon = 'http://wow.zamimg.com/images/classes/symbols/icon-deathknight.png'
+          $classIcon = 'http://wow.zamimg.com/images/classes/symbols/icon-deathknight.png';
           break;
         case 7:
           $className = 'Shaman';
           $classColor = '#0070DE';
-          $classIcon = 'http://wow.zamimg.com/images/classes/symbols/icon-shaman.png'
+          $classIcon = 'http://wow.zamimg.com/images/classes/symbols/icon-shaman.png';
           break;
         case 8:
           $className = 'Mage';
           $classColor = '#69CCF0';
-          $classIcon = 'http://wow.zamimg.com/images/classes/symbols/icon-mage.png'
+          $classIcon = 'http://wow.zamimg.com/images/classes/symbols/icon-mage.png';
           break;
         case 9:
           $className = 'Warlock';
           $classColor = '#9482C9';
-          $classIcon = 'http://wow.zamimg.com/images/classes/symbols/icon-warlock.png'
+          $classIcon = 'http://wow.zamimg.com/images/classes/symbols/icon-warlock.png';
           break;
         case 10:
           $className = 'Monk';
           $classColor = '#00FF96';
-          $classIcon = 'http://wow.zamimg.com/images/classes/symbols/icon-monk.png'
+          $classIcon = 'http://wow.zamimg.com/images/classes/symbols/icon-monk.png';
           break;
         case 11:
           $className = 'Druid';
           $classColor = '#FF7D0A';
-          $classIcon = 'http://wow.zamimg.com/images/classes/symbols/icon-druid.png'
+          $classIcon = 'http://wow.zamimg.com/images/classes/symbols/icon-druid.png';
           break;
         case 12:
           $className = 'Demon Hunter';
           $classColor = '#A330C9';
-          $classIcon = 'http://wow.zamimg.com/images/classes/symbols/icon-demonhunter.png'
+          $classIcon = 'http://wow.zamimg.com/images/classes/symbols/icon-demonhunter.png';
           break;
         default:
           break;
