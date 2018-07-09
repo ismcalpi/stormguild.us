4b51ac323dc5cc28c0bcf47a83069f24bce9b3ae
usumcia <imcalpine@scor.com>
2018-07-09 14:52:18 -0400

    Testing

--- roster2.php
+++ roster2.php
@@ -16,78 +16,71 @@
 
             $classList = $roster -> get_classList();
             foreach ($classList as $class) {
-
-              #echo "<p>".$class[0].", ".$class[1].", ".$class[2]."</p>";
           ?>
             <div class="row">
-              <div class="col-lg-2 col-md-3 col-12">
-                <img class="g-width-100 g-height-100" src="<?php echo $class[2]; ?>">
-                <!--<h3 style="text-center:center;color:<?php echo $class[2]; ?>;"><?php echo $class[0]; ?></h3> -->
+              <div class="col-12">
+                <h3 style="text-center:center;color:<?php echo $class[2]; ?>;"><img src="<?php echo $class[2]; ?>"><?php echo $class[0]; ?></h3>
               </div>
-              <div class="col-lg-10 col-md-9 col-12">
-                <div class="row">
-                  <?php
-                  $results = $roster -> get_classResult($class);
-                  foreach ($results as $result) {
-                    $rank_name = $roster -> get_guildRank($result['rank']);
-                  ?>
-                  <div class='col-lg-4 col-md-6 col-12 g-mb-30'>
-                    <figure class="u-block-hover u-shadow-v19 g-bg-white g-rounded-4 g-pa-15 g-brd-black g-brd-2 g-brd-around">
-                      <div class="d-flex justify-content-start">
-                        <img class="g-width-60 g-height-60 rounded-circle g-mr-15" src="http://render-us.worldofwarcraft.com/character/<?php echo $result['thumbnail']; ?>">
-                        <!-- Figure Info -->
-                        <div class="d-block">
-                          <div class="g-mb-5">
-                            <h4 class="h5 g-mb-0"><?php echo $result['name']; ?></h4>
-                            <em class="d-block g-color-primary g-font-style-normal g-font-size-default"><?php echo $rank_name ?></em>
-                            <!--<img width="20" height="20" class="img-fluid g-mr-10" src="https://wow.zamimg.com/images/wow/icons/large/<?php echo $result['spec_icon'] ?>.jpg">-->
-                          </div>
-                        </div>
-                        <!-- End Figure Info -->
-
-                        <!-- Figure Caption -->
-                        <figcaption style="background-color:rgba(0,0,0,.5)" class="u-block-hover__additional--fade g-pa-30">
-                          <div class="u-block-hover__additional--fade u-block-hover__additional--fade-down g-flex-middle">
-                            <!-- Figure Social Icons -->
-                            <ul class="list-inline text-center g-flex-middle-item">
-                              <li class="list-inline-item align-middle g-mx-7 g-color-white">
-                                <a target="_blank" href="https://worldofwarcraft.com/en-us/character/stormrage/<?php echo urlencode($result['name']); ?>" class="g-color-white">
-                                  <span class="u-icon-v1 u-icon-size--xs g-mr-5">
-                                    <img width="30" height="30" src="assets/img/logo.armory.png">
-                                  </span>
-                                  Armory
-                                </a>
-                              </li>
-                              <li class="list-inline-item align-middle g-mx-7 g-color-white">
-                                <a target="_blank" href="https://www.warcraftlogs.com/character/us/stormrage/<?php echo urlencode($result['name']); ?>" class="g-color-white">
-                                  <span class="u-icon-v1 u-icon-size--xs g-mr-5">
-                                    <img width="30" height="30" src="assets/img/logo.warlogs.png">
-                                  </span>
-                                  Logs
-                                </a>
-                              </li>
-                              <li class="list-inline-item align-middle g-mx-7">
-                                <a target="_blank" href="https://raider.io/characters/us/stormrage/<?php echo urlencode($result['name']); ?>" class="g-color-white">
-                                  <span class="u-icon-v1 u-icon-size--xs g-mr-5">
-                                    <img width="30" height="30" src="assets/img/logo.raiderio.png">
-                                  </span>
-                                  Raider.IO
-                                </a>
-                              </li>
-                            </ul>
-                            <!-- End Figure Social Icons -->
-                          </div>
-                        </figcaption>
-                        <!-- End Figure Caption -->
-
+              <?php
+              $results = $roster -> get_classResult($class);
+              foreach ($results as $result) {
+                $rank_name = $roster -> get_guildRank($result['rank']);
+              ?>
+              <div class='col-lg-4 col-md-6 col-12 g-mb-30'>
+                <figure class="u-block-hover u-shadow-v19 g-bg-white g-rounded-4 g-pa-15 g-brd-black g-brd-2 g-brd-around">
+                  <div class="d-flex justify-content-start">
+                    <img class="g-width-60 g-height-60 rounded-circle g-mr-15" src="http://render-us.worldofwarcraft.com/character/<?php echo $result['thumbnail']; ?>">
+                    <!-- Figure Info -->
+                    <div class="d-block">
+                      <div class="g-mb-5">
+                        <h4 class="h5 g-mb-0"><?php echo $result['name']; ?></h4>
+                        <em class="d-block g-color-primary g-font-style-normal g-font-size-default"><?php echo $rank_name ?></em>
+                        <!--<img width="20" height="20" class="img-fluid g-mr-10" src="https://wow.zamimg.com/images/wow/icons/large/<?php echo $result['spec_icon'] ?>.jpg">-->
                       </div>
-                    </figure>
+                    </div>
+                    <!-- End Figure Info -->
+
+                    <!-- Figure Caption -->
+                    <figcaption style="background-color:rgba(0,0,0,.5)" class="u-block-hover__additional--fade g-pa-30">
+                      <div class="u-block-hover__additional--fade u-block-hover__additional--fade-down g-flex-middle">
+                        <!-- Figure Social Icons -->
+                        <ul class="list-inline text-center g-flex-middle-item">
+                          <li class="list-inline-item align-middle g-mx-7 g-color-white">
+                            <a target="_blank" href="https://worldofwarcraft.com/en-us/character/stormrage/<?php echo urlencode($result['name']); ?>" class="g-color-white">
+                              <span class="u-icon-v1 u-icon-size--xs g-mr-5">
+                                <img width="30" height="30" src="assets/img/logo.armory.png">
+                              </span>
+                              Armory
+                            </a>
+                          </li>
+                          <li class="list-inline-item align-middle g-mx-7 g-color-white">
+                            <a target="_blank" href="https://www.warcraftlogs.com/character/us/stormrage/<?php echo urlencode($result['name']); ?>" class="g-color-white">
+                              <span class="u-icon-v1 u-icon-size--xs g-mr-5">
+                                <img width="30" height="30" src="assets/img/logo.warlogs.png">
+                              </span>
+                              Logs
+                            </a>
+                          </li>
+                          <li class="list-inline-item align-middle g-mx-7">
+                            <a target="_blank" href="https://raider.io/characters/us/stormrage/<?php echo urlencode($result['name']); ?>" class="g-color-white">
+                              <span class="u-icon-v1 u-icon-size--xs g-mr-5">
+                                <img width="30" height="30" src="assets/img/logo.raiderio.png">
+                              </span>
+                              Raider.IO
+                            </a>
+                          </li>
+                        </ul>
+                        <!-- End Figure Social Icons -->
+                      </div>
+                    </figcaption>
+                    <!-- End Figure Caption -->
+
                   </div>
-                  <?php
-                  }
-                  ?>
-                </div>
+                </figure>
               </div>
+              <?php
+              }
+              ?>
             </div>
           <?php
             }
