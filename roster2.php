b06c58d2421cace055ef5b2be085e616c90a6bde
ismcalpi <ismcalpi@gmail.com>
2018-07-10 05:09:21 -0400

    test

--- roster2.php
+++ roster2.php
@@ -20,67 +20,58 @@
               <div class="col-12">
                 <h3 style="color:<?php echo $class[1]; ?>;"><img style="margin-right:10px;" src="<?php echo $class[2]; ?>"><?php echo $class[0]; ?></h3>
               </div>
-              <div class="row">
-                <?php
-                $results = $roster -> get_classResult($class);
-                foreach ($results as $result) {
-                  $rank_name = $roster -> get_guildRank($result['rank']);
-                ?>
-                <div class='col-lg-3 col-md-4 col-6 g-mb-30'>
-                  <figure class="u-block-hover u-shadow-v19 g-bg-white g-rounded-4 g-pa-5 g-brd-black g-brd-1 g-brd-around">
-                    <div class="d-flex justify-content-start">
-                      <img class="g-width-60 g-height-60 rounded-circle" src="http://render-us.worldofwarcraft.com/character/<?php echo $result['thumbnail']; ?>">
-                      <img class="g-width-20 g-height-20 rounded-circle g-mr-10" src="https://wow.zamimg.com/images/wow/icons/large/<?php echo $result['spec_icon'] ?>.jpg">
-                      <!-- Figure Info -->
-                      <div class="d-block">
-                        <div class="g-mb-5 g-mt-5">
-                          <h4 class="h5 g-mb-0"><?php echo $result['name']; ?></h4>
-                          <em class="d-block g-color-primary g-font-style-normal g-font-size-default"><?php echo $rank_name ?></em>
-                        </div>
+              <?php
+              $results = $roster -> get_classResult($class);
+              foreach ($results as $result) {
+                $rank_name = $roster -> get_guildRank($result['rank']);
+              ?>
+              <div class='col-lg-3 col-md-4 col-6 g-mb-30'>
+                <figure class="u-block-hover u-shadow-v19 g-bg-white g-rounded-4 g-pa-5 g-brd-black g-brd-1 g-brd-around">
+                  <div class="d-flex justify-content-start">
+                    <img class="g-width-60 g-height-60 rounded-circle" src="http://render-us.worldofwarcraft.com/character/<?php echo $result['thumbnail']; ?>">
+                    <img class="g-width-20 g-height-20 rounded-circle g-mr-10" src="https://wow.zamimg.com/images/wow/icons/large/<?php echo $result['spec_icon'] ?>.jpg">
+                    <div class="d-block">
+                      <div class="g-mb-5 g-mt-5">
+                        <h4 class="h5 g-mb-0"><?php echo $result['name']; ?></h4>
+                        <em class="d-block g-color-primary g-font-style-normal g-font-size-default"><?php echo $rank_name ?></em>
                       </div>
-                      <!-- End Figure Info -->
-
-                      <!-- Figure Caption -->
-                      <figcaption style="background-color:rgba(0,0,0,.5)" class="u-block-hover__additional--fade g-pa-30">
-                        <div class="u-block-hover__additional--fade u-block-hover__additional--fade-down g-flex-middle">
-                          <!-- Figure Social Icons -->
-                          <ul class="list-inline text-center g-flex-middle-item">
-                            <li class="list-inline-item align-middle g-mx-7 g-color-white">
-                              <a target="_blank" alt="WoW Armory" href="https://worldofwarcraft.com/en-us/character/stormrage/<?php echo urlencode($result['name']); ?>" class="g-color-white">
-                                <span class="u-icon-v1 u-icon-size--xs g-mr-5">
-                                  <img width="30" height="30" src="assets/img/logo.armory.png">
-                                </span>
-                                <!-- Armory -->
-                              </a>
-                            </li>
-                            <li class="list-inline-item align-middle g-mx-7 g-color-white">
-                              <a target="_blank" alt="Warcraft Logs" href="https://www.warcraftlogs.com/character/us/stormrage/<?php echo urlencode($result['name']); ?>" class="g-color-white">
-                                <span class="u-icon-v1 u-icon-size--xs g-mr-5">
-                                  <img width="30" height="30" src="assets/img/logo.warlogs.png">
-                                </span>
-                                <!-- Logs -->
-                              </a>
-                            </li>
-                            <li class="list-inline-item align-middle g-mx-7">
-                              <a target="_blank" alt="Raider.IO" href="https://raider.io/characters/us/stormrage/<?php echo urlencode($result['name']); ?>" class="g-color-white">
-                                <span class="u-icon-v1 u-icon-size--xs g-mr-5">
-                                  <img width="30" height="30" src="assets/img/logo.raiderio.png">
-                                </span>
-                                <!-- Raider.IO -->
-                              </a>
-                            </li>
-                          </ul>
-                          <!-- End Figure Social Icons -->
-                        </div>
-                      </figcaption>
-                      <!-- End Figure Caption -->
                     </div>
-                  </figure>
-                </div>
-                <?php
-                }
-                ?>
+                    <figcaption style="background-color:rgba(0,0,0,.5)" class="u-block-hover__additional--fade g-pa-30">
+                      <div class="u-block-hover__additional--fade u-block-hover__additional--fade-down g-flex-middle">
+                        <ul class="list-inline text-center g-flex-middle-item">
+                          <li class="list-inline-item align-middle g-mx-7 g-color-white">
+                            <a target="_blank" alt="WoW Armory" href="https://worldofwarcraft.com/en-us/character/stormrage/<?php echo urlencode($result['name']); ?>" class="g-color-white">
+                              <span class="u-icon-v1 u-icon-size--xs g-mr-5">
+                                <img width="30" height="30" src="assets/img/logo.armory.png">
+                              </span>
+                              <!-- Armory -->
+                            </a>
+                          </li>
+                          <li class="list-inline-item align-middle g-mx-7 g-color-white">
+                            <a target="_blank" alt="Warcraft Logs" href="https://www.warcraftlogs.com/character/us/stormrage/<?php echo urlencode($result['name']); ?>" class="g-color-white">
+                              <span class="u-icon-v1 u-icon-size--xs g-mr-5">
+                                <img width="30" height="30" src="assets/img/logo.warlogs.png">
+                              </span>
+                              <!-- Logs -->
+                            </a>
+                          </li>
+                          <li class="list-inline-item align-middle g-mx-7">
+                            <a target="_blank" alt="Raider.IO" href="https://raider.io/characters/us/stormrage/<?php echo urlencode($result['name']); ?>" class="g-color-white">
+                              <span class="u-icon-v1 u-icon-size--xs g-mr-5">
+                                <img width="30" height="30" src="assets/img/logo.raiderio.png">
+                              </span>
+                              <!-- Raider.IO -->
+                            </a>
+                          </li>
+                        </ul>
+                      </div>
+                    </figcaption>
+                  </div>
+                </figure>
               </div>
+              <?php
+              }
+              ?>
             </div>
           <?php
             }
