231a908c00bf37ea9190ce5bd2427be68176f259
usumcia <imcalpine@scor.com>
2018-07-24 15:53:38 -0400

    Fix

--- about2.php
+++ about2.php
@@ -15,7 +15,7 @@
 				<section class="g-bg-white g-brd-around g-brd-black g-brd-2">
 
 					<div class="u-heading-v1-1 g-bg-main g-brd-blue g-mb-20">
-						<h2 class="h3 u-heading-v1__title"><?php echo $section['title']; ?></h2>
+						<h2 class="h3 u-heading-v1__title text-center"><?php echo $section['title']; ?></h2>
 					</div>
 
 					<?php echo $section['content']; ?>
