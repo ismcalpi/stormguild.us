0888b9c0a5edc7db26081442d61d760490f56895
ismcalpi <ismcalpi@gmail.com>
2018-07-25 08:12:34 -0400

    Fix

--- about2.php
+++ about2.php
@@ -13,8 +13,8 @@
 						$sections = $db -> read_select("select * from stormguild.about_us where is_active = 1 order by a_order asc");
 						foreach($sections as $section) {
 					?>
-					<div class="g-pa-10">
-						<section class="<?php echo $section['size']; ?> g-bg-white g-brd-around g-brd-blue g-brd-2 g-pa-20">
+					<div class="<?php echo $section['size']; ?> g-pa-5">
+						<section class=" g-bg-white g-brd-around g-brd-blue g-brd-2 g-pa-20">
 
 							<div class="u-heading-v1-1 g-bg-main g-brd-blue g-mb-20 text-center">
 								<h2 class="h3 u-heading-v1__title g-mx-20"><?php echo $section['title']; ?></h2>
