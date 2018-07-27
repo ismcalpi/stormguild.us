4cdf88edd4e7d8cccd0bd95d6133a12f3bdfea53
ismcalpi <ismcalpi@gmail.com>
2018-07-27 13:01:49 -0400

    Update

--- templates/admin.aboutus.php
+++ templates/admin.aboutus.php
@@ -12,10 +12,18 @@
 
       <div class="col-md-4 col-12">
 
+
+
       </div>
 
       <div class="col-md-8 col-12">
+        <label for="title">Section Title</label>
+        <input id="name" class="form-control form-control-md rounded-0 g-ma-10"
+          type="text" name="title" placeholder="<?php echo $section['title']; ?>"></input>
 
+        <label for="content">Section Content (Accepts HTML)</label>
+        <textarea id="content" class="form-control form-control-md rounded-0 g-ma-10"
+          name="content"><?php echo $section['content']; ?></textarea>
       </div>
 
     </div>
