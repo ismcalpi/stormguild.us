f8d75b332664dd8f9d4162c217a825bb1a8f12c7
ismcalpi <ismcalpi@gmail.com>
2018-07-27 13:23:08 -0400

    Formatting

--- templates/admin.aboutus.php
+++ templates/admin.aboutus.php
@@ -14,15 +14,15 @@
       <div class="col-md-3 col-12">
 
         <label for="order">Order</label>
-        <input id="order" class="form-control form-control-md rounded-0 g-ma-10"
+        <input id="order" class="form-control form-control-md rounded-0"
           type="number" name="order" value="<?php echo $section['a_order']; ?>"></input>
 
         <label for="size">Size</label>
-        <input id="size" class="form-control form-control-md rounded-0 g-ma-10"
+        <input id="size" class="form-control form-control-md rounded-0"
           type="number" name="size" value="<?php echo $section['size']; ?>"></input>
 
         <label for="active">Is Active</label>
-        <input id="active" class="form-control form-control-md rounded-0 g-ma-10"
+        <input id="active" class="form-control form-control-md rounded-0"
           type="number" name="active" value="<?php echo $section['is_active']; ?>"></input>
 
       </div>
@@ -30,19 +30,19 @@
       <div class="col-md-9 col-12">
 
         <label for="title">Section Title</label>
-        <input id="name" class="form-control form-control-md rounded-0 g-ma-10"
+        <input id="name" class="form-control form-control-md rounded-0"
           type="text" name="title" value="<?php echo $section['title']; ?>"></input>
 
         <label for="content">Section Content (Accepts HTML)</label>
-        <textarea id="content" class="form-control form-control-md rounded-0 g-ma-10"
+        <textarea id="content" class="form-control form-control-md rounded-0"
           name="content"><?php echo $section['content']; ?></textarea>
 
       </div>
 
       <div class="col-12">
 
-        <input type="submit" class="btn btn-md u-btn-primary g-ma-5" value="Update"></input>
-        <input type="submit" class="btn btn-md u-btn-red g-ma-5" value="Delete"></input>
+        <input type="submit" class="btn btn-md u-btn-primary g-ma-10" value="Update"></input>
+        <input type="submit" class="btn btn-md u-btn-red g-ma-10" value="Delete"></input>
 
       </div>
 
