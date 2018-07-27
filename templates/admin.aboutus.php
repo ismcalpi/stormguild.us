4253cc182944d803c4683a7c6d161bd37f441943
ismcalpi <ismcalpi@gmail.com>
2018-07-27 13:04:06 -0400

    Update

--- templates/admin.aboutus.php
+++ templates/admin.aboutus.php
@@ -1,4 +1,5 @@
 
+<h1 class="h1">Work in Progress</h1>
 
   <?php
   include_once 'library/class.database.php';
@@ -6,9 +7,9 @@
   $sections = $db -> read_select("SELECT * FROM stormguild.about_us ORDER BY a_order ASC");
   foreach($sections as $section) {
   ?>
-  <h1 class="h1">Work in Progress</h1>
+
   <form enctype="multipart/form-data" method="post" action="library/action.admin.aboutus.php">
-    <div class="row g-brd-around g-brd-2 g-brd-blue">
+    <div class="row g-brd-around g-brd-2 g-brd-blue g-ma-30">
 
       <div class="col-md-4 col-12">
 
@@ -19,7 +20,7 @@
       <div class="col-md-8 col-12">
         <label for="title">Section Title</label>
         <input id="name" class="form-control form-control-md rounded-0 g-ma-10"
-          type="text" name="title" placeholder="<?php echo $section['title']; ?>"></input>
+          type="text" name="title" value="<?php echo $section['title']; ?>"></input>
 
         <label for="content">Section Content (Accepts HTML)</label>
         <textarea id="content" class="form-control form-control-md rounded-0 g-ma-10"
