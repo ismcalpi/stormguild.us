cb06b5d9356d9b525d8b7835226159548fd61f0b
usumcia <imcalpine@scor.com>
2018-09-25 14:53:41 -0400

    Formatting Fixes

--- sales_new.php
+++ sales_new.php
@@ -20,9 +20,8 @@
               <div class="u-block-hover__additional g-flex-middle g-z-index-2 g-brd-around g-brd-2 g-brd-white-opacity-0_3 g-pa-15 g-ma-20">
                 <div class="g-flex-middle-item">
                   <h3 class="h4 d-inline-block">Uldir Heroic 8/8</h3>
-                  <span class="d-block g-font-weight-600 g-font-size-40 text-uppercase g-mb-20">1,000,000 Gold</span>
-                  <a class="js-fancybox btn btn-md u-btn-primary g-font-weight-600 g-font-size-12 text-uppercase" href="javascript:;" data-fancybox="lightbox-gallery-01-1" data-src="../../assets/img-temp/400x270/img2.jpg">Purchase Now
-                  </a>
+                  <span class="d-block g-font-weight-600 g-font-size-20 text-uppercase g-mb-20">1,000,000G</span>
+                  <a class="btn btn-md u-btn-primary g-font-weight-600 g-font-size-12 text-uppercase">Purchase Now</a>
                 </div>
               </div>
             </article>
@@ -40,9 +39,8 @@
               <div class="u-block-hover__additional g-flex-middle g-z-index-2 g-brd-around g-brd-2 g-brd-white-opacity-0_3 g-pa-15 g-ma-20">
                 <div class="g-flex-middle-item">
                   <h3 class="h4 d-inline-block">Uldir Normal 8/8</h3>
-                  <span class="d-block g-font-weight-600 g-font-size-40 text-uppercase g-mb-20">1,000,000 Gold</span>
-                  <a class="js-fancybox btn btn-md u-btn-primary g-font-weight-600 g-font-size-12 text-uppercase" href="javascript:;" data-fancybox="lightbox-gallery-01-1" data-src="../../assets/img-temp/400x270/img2.jpg">Purchase Now
-                  </a>
+                  <span class="d-block g-font-weight-600 g-font-size-20 text-uppercase g-mb-20">1,000,000G</span>
+                  <a class="btn btn-md u-btn-primary g-font-weight-600 g-font-size-12 text-uppercase">Purchase Now</a>
                 </div>
               </div>
             </article>
@@ -60,9 +58,8 @@
               <div class="u-block-hover__additional g-flex-middle g-z-index-2 g-brd-around g-brd-2 g-brd-white-opacity-0_3 g-pa-15 g-ma-20">
                 <div class="g-flex-middle-item">
                   <h3 class="h4 d-inline-block">G'Huun Heroic</h3>
-                  <span class="d-block g-font-weight-600 g-font-size-40 text-uppercase g-mb-20">1,000,000 Gold</span>
-                  <a class="js-fancybox btn btn-md u-btn-primary g-font-weight-600 g-font-size-12 text-uppercase" href="javascript:;" data-fancybox="lightbox-gallery-01-1" data-src="../../assets/img-temp/400x270/img2.jpg">Purchase Now
-                  </a>
+                  <span class="d-block g-font-weight-600 g-font-size-20 text-uppercase g-mb-20">1,000,000G</span>
+                  <a class="btn btn-md u-btn-primary g-font-weight-600 g-font-size-12 text-uppercase">Purchase Now</a>
                 </div>
               </div>
             </article>
