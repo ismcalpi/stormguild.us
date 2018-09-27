479d5a7b77c50e2f90014c1e30123b1852f39155
ismcalpi <ismcalpi@gmail.com>
2018-09-27 15:05:37 -0400

    Add auto decline script

---
+++ library/http.update.autodecline.php
@@ -0,0 +1,31 @@
+<?php
+
+  include '../library/class.database.php';
+  $db = new database();
+
+  $sql = "select * from stormguild.application where create_datetime < date_sub(now(),interval 7 day) and status = 'open'";
+  $applications = $db -> read_row($sql);
+
+  for each ($applications as $application) {
+
+    // set post fields
+    $post = [
+        'appid' => $application['application_id'],
+        'appname' => $application['charName'],
+        'status'   => 'declined',
+    ];
+
+    $ch = curl_init('http://www.stormguild.us/library/action.application.decision.php');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+
+    // execute!
+    $response = curl_exec($ch);
+
+    // close the connection, release resources used
+    curl_close($ch);
+
+    echo 'Application ID: '.$application['application_id'].' | Char Name: '.$application['charName']. ' | Has been auto-declined due to application being over 7 days open.';
+
+  }
+?>
