eb2f4c4e1f0e9c02b3e6e73daecbeaa1a172ecf9
ismcalpi <ismcalpi@gmail.com>
2018-10-16 08:07:39 -0400

    Fix

--- library/action.test.php
+++ library/action.test.php
@@ -1,5 +1,5 @@
 
 <?php
-  $message = $_SERVER['HTTP_REFERER'];
-  echo $message;
+  $my_referer = isset($_POST['referer']) ? trim($_POST['referer']) : (isset($_SERVER['HTTP_REFERER']) ? base64_encode($_SERVER['HTTP_REFERER']) : false);;
+  echo $my_referer;
 ?>
