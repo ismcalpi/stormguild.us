8b417c1ff7d27227186d804dff7cd6067d06ee85
ismcalpi <ismcalpi@gmail.com>
2019-01-25 11:15:56 -0500

    Update twitch.php

--- twitch.php
+++ twitch.php
@@ -2,11 +2,6 @@
 <html lang="en">
 	<?php include 'templates/all.user.php' ?>
 	<?php include 'templates/twitch.head.php' ?>
-	<?php
-		include_once 'library/function.twitch.php';
-		include_once 'library/class.database.php';
-		$db = new database();
-	?>
 	<body class="main-body">
 		<main>
 			<?php include 'templates/all.navbar.php' ?>
@@ -16,23 +11,23 @@
 						<h2 class="h1 text-uppercase g-font-weight-300 u-heading-v2__title"><strong><i class="fa fa-twitch u-tab-line-icon-pro g-mr-3 g-color-purple"></i> Storm Streamers</strong></h2>
 					</div>
 					<?php
+						include_once 'library/class.database.php';
+						$db = new database();
+
 						$results = $db -> read_select("select * from stormguild.streamers where is_active = 1");
 						foreach($results as $result) {
-							$twitchStatus = getTwitchStatus($result['username']);
-							$user = getUserJSON($result['username']);
-							$channel = getChannelJSON($result['username']);
 					?>
 
 					<div class="col-md-3 col-6">
 						<div class="u-shadow-v19 g-brd-around g-brd-purple g-brd-3 text-center rounded g-pb-20 g-px-15 g-my-50 g-mx-10" style="background-color: rgba(100, 65, 165, .2);">
-				      <img class="g-brd-3 g-brd-style-solid g-brd-purple g-width-100 g-height-100 rounded-circle g-pull-50x-up" src="<?php echo $user['users'][0]['logo']; ?>" alt="Image Description">
+				      <img class="g-brd-3 g-brd-style-solid g-brd-purple g-width-100 g-height-100 rounded-circle g-pull-50x-up" src="<?php echo $result['logo']; ?>" alt="Image Description">
 				      <div class="g-mt-minus-20">
-				        <h4 class="h6 g-color-purple g-font-weight-600 text-uppercase g-mb-5"><?php echo $user['users'][0]['display_name']; ?></h4>
-				        <em class="d-block g-color-black g-font-style-normal g-font-size-13 g-mb-20"><?php echo $channel['status']; ?></em>
-								<?php if($twitchStatus){ ?>
-									<a target="_blank" href="<?php echo $channel['url']; ?>" class="btn u-btn-sm u-btn-primary">Live Now!</a>
+				        <h4 class="h6 g-color-purple g-font-weight-600 text-uppercase g-mb-5"><?php echo $result['displayname']; ?></h4>
+				        <em class="d-block g-color-black g-font-style-normal g-font-size-13 g-mb-20"><?php echo $result['status']; ?></em>
+								<?php if($result['online'] == 1){ ?>
+									<a target="_blank" href="<?php echo $result['url']; ?>" class="btn u-btn-sm u-btn-primary">Live Now!</a>
 								<?php } else { ?>
-									<a target="_blank" href="<?php echo $channel['url']; ?>" class="btn u-btn-sm u-btn-bluegray">Offline</a>
+									<a target="_blank" href="<?php echo $result['url']; ?>" class="btn u-btn-sm u-btn-bluegray">Offline</a>
 								<?php }?>
 				      </div>
 				    </div>
