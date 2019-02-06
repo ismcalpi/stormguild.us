b2bd93478b677390423243a4c9c6399002fe6071
Ian McAlpine <imcalpine@scorvelogica.com>
2019-02-06 12:55:58 -0500

    Cleanup

---
+++ assets/php/class/database.php
@@ -0,0 +1,63 @@
+<?php
+	class database {
+
+		protected static $connection;
+
+		private function connect($type = 'read') {
+			if(!isset(self::$connection)) {
+				$iniPath = $_SERVER['DOCUMENT_ROOT'].'/config/database.ini';
+				$config = parse_ini_file($iniPath);
+				switch ($type) {
+					case 'write':
+                        self::$connection = new mysqli($config['db_address'],$config['write_uid'],$config['write_pwd'],$config['dbname']);
+						break;
+					case 'read':
+                        self::$connection = new mysqli($config['db_address'],$config['read_uid'],$config['read_pwd'],$config['dbname']);
+						break;
+				}
+			}
+			if(self::$connection === false) {
+				return false;
+			}
+			return self::$connection;
+		}
+
+		public function readResults($query) {
+            $rows = array();
+			$connection = $this -> connect('read');
+            $result = $connection -> query($query);
+			if($result === false) {
+                return $connection -> error;
+			}
+			while ($row = $result -> fetch_assoc()) {
+				$rows[] = $row;
+			}
+			return $rows;
+		}
+
+        public function readRow($query) {
+            $connection = $this -> connect('read');
+            $result = $connection -> query($query);
+            if (!$result) {
+                return $connection -> error;
+            }
+            $row = $result -> fetch_assoc();
+            return $row;
+        }
+
+        public function writeQuery($query) {
+            $connection = $this -> connect('write');
+            $result = $connection -> query($query);
+            if($result === false) {
+                return $connection -> error;
+            }
+            return $connection -> sqlstate;
+        }
+
+		public function quote($value) {
+			$connection = $this -> connect();
+			return "'" . $connection -> real_escape_string($value) . "'";
+		}
+	}
+
+?>
---
+++ assets/php/class/discord.php
@@ -0,0 +1,17 @@
+<?php
+	class discord {
+    public function discord_message($user,$message,$channel) {
+      $iniPath = $_SERVER['DOCUMENT_ROOT'].'/config/discord.ini';
+      $config = parse_ini_file($iniPath);
+
+      $data = array("content" => $message, "username" => $user);
+      $hookurl = $config[$channel];
+      $curl = curl_init($hookurl);
+      curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
+      curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data));
+      curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+      return curl_exec($curl);
+    }
+	}
+
+?>
