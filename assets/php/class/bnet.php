45f749df18627a3ace88affa6622dc3bcaa6301c
ismcalpi <ismcalpi@gmail.com>
2019-02-07 22:32:37 -0500

    Create bnet.php

---
+++ assets/php/class/bnet.php
@@ -0,0 +1,19 @@
+<?php
+  class bnet {
+
+    protected $iniPath = $_SERVER['DOCUMENT_ROOT'].'/config/bnet.ini';
+
+    function get_token() {
+
+    }
+
+    function get_roster() {
+
+    }
+
+    function get_progression() {
+
+    }
+
+  }
+?>
