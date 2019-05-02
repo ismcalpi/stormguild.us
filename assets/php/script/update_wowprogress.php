881e254124fbfddee8b9918a7a278374dd9c0ab4
ismcalpi <ismcalpi@gmail.com>
2019-05-02 14:45:26 -0400

    Update update_wowprogress.php

--- assets/php/script/update_wowprogress.php
+++ assets/php/script/update_wowprogress.php
@@ -8,13 +8,13 @@
   $area_rank = $json->area_rank;
   $realm_rank = $json->realm_rank;
 
-  $current = $db -> read_row('SELECT * FROM stormguild.guild_rank');
+  $current = $db -> readResults('SELECT * FROM stormguild.guild_rank');
   if(!$current) {
     echo "Creating New Row";
-    $sql = $db -> write_query('INSERT INTO stormguild.guild_rank VALUES(NULL,'.$world_rank.','.$area_rank.','.$realm_rank.',now(),now())');
+    $sql = $db -> writeQuery('INSERT INTO stormguild.guild_rank VALUES(NULL,'.$world_rank.','.$area_rank.','.$realm_rank.',now(),now())');
   } else if ($current['world_rank'] != $world_rank || $current['area_rank'] != $area_rank || $current['realm_rank'] != $realm_rank) {
     echo "Updating Existing Row";
-    $sql = $db -> write_query('UPDATE stormguild.guild_rank set world_rank = '.$world_rank.', area_rank = '.$area_rank.', realm_rank = '.$realm_rank.', update_datetime = now()');
+    $sql = $db -> writeQuery('UPDATE stormguild.guild_rank set world_rank = '.$world_rank.', area_rank = '.$area_rank.', realm_rank = '.$realm_rank.', update_datetime = now()');
   } else {
     echo "No Updates Required";
   }
