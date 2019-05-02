619828ca349fdc0bf6550ade33275707b7870e62
ismcalpi <ismcalpi@gmail.com>
2019-05-02 14:58:50 -0400

    Update update_wowprogress.php

--- assets/php/script/update_wowprogress.php
+++ assets/php/script/update_wowprogress.php
@@ -4,7 +4,6 @@
   $db = new database();
 
   $json = json_decode(file_get_contents('https://www.wowprogress.com/guild/us/stormrage/storm/json_rank'));
-  $world_rank = $json->world_rank;
   $area_rank = $json->area_rank;
   $realm_rank = $json->realm_rank;
 
@@ -14,7 +13,7 @@
     $sql = $db -> writeQuery('INSERT INTO stormguild.guild_rank VALUES(NULL,'.$world_rank.','.$area_rank.','.$realm_rank.',now(),now())');
   } else if ($current['world_rank'] != $world_rank || $current['area_rank'] != $area_rank || $current['realm_rank'] != $realm_rank) {
     echo "Updating Existing Row <br />";
-    $sql = $db -> writeQuery('UPDATE stormguild.guild_rank set world_rank = '.$world_rank.', area_rank = '.$area_rank.', realm_rank = '.$realm_rank.', update_datetime = now()');
+    $sql = $db -> writeQuery('UPDATE stormguild.guild_rank set area_rank = '.$area_rank.', realm_rank = '.$realm_rank.', update_datetime = now()');
   } else {
     echo "No Updates Required <br />";
   }
@@ -22,12 +21,14 @@
   $tierlist = $db -> readResults("select raid_id, tier_number from stormguild.raid where tier_number > 0 and is_active = 1");
   foreach ($tierlist as $tier) {
     echo "Dealing with Tier ".$tier['tier_number']. " <br />";
-    $link = 'https://www.wowprogress.com/guild/us/stormrage/Storm/rating.'.$tier['tier_number'].'/json_rank';
+    $link = 'https://www.wowprogress.com/guild/us/stormrage/Storm/rating.tier'.$tier['tier_number'].'/json_rank';
     echo $link." <br />";
     $json = json_decode(file_get_contents($link));
     $realm_rank = $json->realm_rank;
     $us_rank = $json->area_rank;
-    $update = $db -> writeQuery("update stormguild.raid set realm_rank = ".$realm_rank.", us_rank = ".$us_rank." where raid_id = ".$tier['raid_id']);
+    $sql = "update stormguild.raid set realm_rank = ".$realm_rank.", us_rank = ".$us_rank." where raid_id = ".$tier['raid_id'];
+    echo $sql." <br />";
+    $update = $db -> writeQuery($sql);
   }
 
 ?>
