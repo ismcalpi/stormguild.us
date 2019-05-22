9f0a40abcd182ea1bb050f1975c94ea9138c8db9
ismcalpi <ismcalpi@gmail.com>
2019-05-22 07:59:19 -0400

    Update discord.php

--- assets/php/class/discord.php
+++ assets/php/class/discord.php
@@ -1,17 +1,6 @@
 <?php
 	class discord {
 
-		function get_hook_url($channel) {
-
-      $iniPath = $_SERVER['DOCUMENT_ROOT'].'/config/discord.ini';
-      $config = parse_ini_file($iniPath);
-
-      return $config[$channel];
-
-      #TODO Error Checking for if the channel is not found or just general error.
-
-    }
-
     public function discord_message($user,$message,$channel) {
 
       $data = array("content" => $message, "username" => $user);
@@ -22,6 +11,17 @@
       curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data));
       curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
       return curl_exec($curl);
+
+    }
+
+		function get_hook_url($channel) {
+
+      $iniPath = $_SERVER['DOCUMENT_ROOT'].'/config/discord.ini';
+      $config = parse_ini_file($iniPath);
+
+      return $config[$channel];
+
+      #TODO Error Checking for if the channel is not found or just general error.
 
     }
 
